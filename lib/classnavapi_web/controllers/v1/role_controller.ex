80cac9562d14f3c0d020b3b78f4533ccc2decb9d
Tyler Witt <tyler@fortyau.com>
2017-10-06 15:53:29 -0500

    Added Get Roles by User endpoint.

--- lib/classnavapi_web/controllers/v1/role_controller.ex
+++ lib/classnavapi_web/controllers/v1/role_controller.ex
@@ -1,17 +1,28 @@
 defmodule ClassnavapiWeb.Api.V1.RoleController do
   use ClassnavapiWeb, :controller
   
+  alias Classnavapi.UserRole
+  alias Classnavapi.Repo
+  alias ClassnavapiWeb.UserRoleView
+
+  import Ecto.Query
+  
   def create(conn, %{"user_id" => user_id, "id" => role_id}) do
 
-    changeset = Classnavapi.UserRole.changeset(%Classnavapi.UserRole{}, %{user_id: user_id, role_id: role_id})
+    changeset = UserRole.changeset(%UserRole{}, %{user_id: user_id, role_id: role_id})
 
-    case Classnavapi.Repo.insert(changeset) do
+    case Repo.insert(changeset) do
       {:ok, user_role} ->
-        render(conn, ClassnavapiWeb.UserRoleView, "show.json", user_role: user_role)
+        render(conn, UserRoleView, "show.json", user_role: user_role)
       {:error, changeset} ->
         conn
         |> put_status(:unprocessable_entity)
         |> render(ClassnavapiWeb.ChangesetView, "error.json", changeset: changeset)
     end
+  end
+
+  def index(conn, %{"user_id" => user_id}) do
+    user_roles = Repo.all(from ur in UserRole, where: ur.user_id == ^user_id)
+    render(conn, UserRoleView, "index.json", user_roles: user_roles)
   end
 end
