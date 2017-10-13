cbadec172d071527c7382afff91e03c01667f6e1
Tyler Witt <tyler@fortyau.com>
2017-10-13 14:25:57 -0500

    Added email validation for school

--- lib/classnavapi/user.ex
+++ lib/classnavapi/user.ex
@@ -3,6 +3,8 @@ defmodule Classnavapi.User do
   import Ecto.Changeset
 
   alias Classnavapi.User
+  alias Classnavapi.Repo
+
 
   schema "users" do
     field :email, :string
@@ -26,13 +28,29 @@ defmodule Classnavapi.User do
     end
   end
 
+  def validate_email(changeset, nil), do: changeset
+  def validate_email(changeset, student_obj) do
+    school = Repo.get(Classnavapi.School, student_obj["school_id"])
+    if (school == nil) do
+      add_error(changeset, :student, "Invalid school")
+    else
+      email_domain = school.email_domain
+      user_email = get_field(changeset, :email)
+      if tl(String.split(email_domain, "@")) != tl(String.split(user_email, "@")) do
+        add_error(changeset, :student, "Invalid email for school.")
+      else
+        changeset
+      end
+    end
+  end
+
   @doc false
   def changeset(%User{} = user, attrs) do
     user
     |> cast(attrs, [:email, :password])
     |> validate_required([:email, :password])
-    |> validate_format(:email, ~r/@/)
     |> unique_constraint(:email)
     |> student_assoc(attrs["student"])
+    |> validate_email(attrs["student"])
   end
 end
