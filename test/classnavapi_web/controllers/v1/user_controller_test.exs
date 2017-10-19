d88b7bdee6417f0701907b3f0a7c6d7213b2360a
Tyler Witt <tyler@fortyau.com>
2017-10-19 15:33:18 -0500

    Index and show user unit tests

--- test/classnavapi_web/controllers/v1/user_controller_test.exs
+++ test/classnavapi_web/controllers/v1/user_controller_test.exs
@@ -10,10 +10,11 @@ defmodule ClassnavapiWeb.Api.V1.UserControllerTest do
   end
 
   test "index/2 responds with all Users", %{jwt: jwt} do
-    users = [ User.changeset_insert(%User{}, %{email: "john@example.com", password: "test"}),
-              User.changeset_insert(%User{}, %{email: "jane@example.com", password: "test"}) ]
-
-    Enum.each(users, &Repo.insert!(&1))
+    john = User.changeset_insert(%User{}, %{email: "john@example.com", password: "test"})
+    |> Repo.insert!
+    
+    jane = User.changeset_insert(%User{}, %{email: "jane@example.com", password: "test"})
+    |> Repo.insert!
 
     response = build_conn()
     |> put_req_header("authorization", "Bearer #{jwt}")
@@ -21,10 +22,24 @@ defmodule ClassnavapiWeb.Api.V1.UserControllerTest do
     |> json_response(200)
 
     expected = [
-        %{ "email" => "john@example.com"},
-        %{ "email" => "jane@example.com"}
+        %{ "email" => "john@example.com", "id" => john.id},
+        %{ "email" => "jane@example.com", "id" => jane.id}
     ]
 
-    assert  Enum.map(response, &(Map.delete(&1, "id"))) == expected
+    assert  response == expected
+  end
+
+  test "show/2 responds with a users id if the user is found", %{jwt: jwt} do
+    user = User.changeset_insert(%User{}, %{email: "test@example.com", password: "test"})
+    |> Repo.insert!
+
+    response = build_conn()
+    |> put_req_header("authorization", "Bearer #{jwt}")
+    |> get(v1_user_path(build_conn(), :index))
+    |> json_response(200)
+
+    expected = [%{ "email" => "test@example.com", "id" => user.id}]
+
+    assert  response == expected
   end
 end
