ec5aa81a040626c76c693a6d61b8530b7e4d971d
Tyler Witt <tyler@fortyau.com>
2017-10-27 15:32:53 -0500

    Added the amount an assignment is worth overall for grade.

--- lib/classnavapi_web/views/assignment_view.ex
+++ lib/classnavapi_web/views/assignment_view.ex
@@ -1,7 +1,10 @@
 defmodule ClassnavapiWeb.AssignmentView do
     use ClassnavapiWeb, :view
 
+    import Ecto.Query
+
     alias ClassnavapiWeb.AssignmentView
+    alias Classnavapi.Repo
 
     def render("index.json", %{assignments: assignments}) do
         render_many(assignments, AssignmentView, "assignment.json")
@@ -12,11 +15,21 @@ defmodule ClassnavapiWeb.AssignmentView do
     end
 
     def render("assignment.json", %{assignment: assignment}) do
+        assignment = assignment |> get_weight_ratio()
+
         %{
             due: assignment.due,
             name: assignment.name,
-            weight_id: assignment.weight_id
+            weight_id: assignment.weight_id,
+            worth: assignment.worth
         }
+    end
+
+    defp get_weight_ratio(assignment) do
+        weight = Repo.get!(Classnavapi.Class.Weight, assignment.weight_id)
+        assignments = Repo.all(from a in Classnavapi.Class.Assignment, where: a.class_id == ^assignment.class_id and a.weight_id == ^Map.get(weight, :id))
+        assignment_count = assignments |> Enum.count(& &1)
+        Map.put(assignment, :worth, Decimal.div(Map.get(weight, :weight), Decimal.new(assignment_count)))
     end
 end
   
