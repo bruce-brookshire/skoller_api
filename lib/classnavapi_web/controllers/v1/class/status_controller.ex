b57689f06637c1ab5a68d66d72e2c8da12d2461f
Tyler Witt <tyler@fortyau.com>
2017-10-31 09:39:46 -0500

    Added endpoint for hub to get number of classes in each status.

--- lib/classnavapi_web/controllers/v1/class/status_controller.ex
+++ lib/classnavapi_web/controllers/v1/class/status_controller.ex
@@ -3,10 +3,29 @@ defmodule ClassnavapiWeb.Api.V1.Class.StatusController do
     
     alias Classnavapi.Class.Status
     alias Classnavapi.Repo
-    alias ClassnavapiWeb.Class.StatusView
+    alias ClassnavapiWeb.Class.StatusView, as: ClassStatusView
+    alias ClassnavapiWeb.Hub.Class.StatusView, as: HubStatusView
+
+    import Ecto.Query
   
     def index(conn, %{}) do
       statuses = Repo.all(Status)
-      render(conn, StatusView, "index.json", statuses: statuses)
+      render(conn, ClassStatusView, "index.json", statuses: statuses)
+    end
+
+    defp get_class_count_by_status(status) do
+      Repo.all(from class in Classnavapi.Class, where: class.class_status_id == ^status.id)
+      |> Enum.count(& &1)
+    end
+
+    defp get_class_status_counts(statuses) do
+      statuses 
+      |> Enum.map(&Map.put(&1, :classes, get_class_count_by_status(&1)))
+    end
+
+    def hub(conn, %{}) do
+      statuses = Repo.all(Status)
+      |> get_class_status_counts
+      render(conn, HubStatusView, "index.json", statuses: statuses)
     end
   end
