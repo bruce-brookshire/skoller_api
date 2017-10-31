bf8b16e74afa5e9b62b4ed3561169dffabbbb6d8
Tyler Witt <tyler@fortyau.com>
2017-10-31 09:53:26 -0500

    Refactored with credo

--- lib/classnavapi_web/controllers/v1/class/status_controller.ex
+++ lib/classnavapi_web/controllers/v1/class/status_controller.ex
@@ -14,18 +14,20 @@ defmodule ClassnavapiWeb.Api.V1.Class.StatusController do
     end
 
     defp get_class_count_by_status(status) do
-      Repo.all(from class in Classnavapi.Class, where: class.class_status_id == ^status.id)
+      classes = Repo.all(from class in Classnavapi.Class, where: class.class_status_id == ^status.id)
+
+      classes
       |> Enum.count(& &1)
     end
 
-    defp get_class_status_counts(statuses) do
+    defp put_class_status_counts(statuses) do
       statuses 
       |> Enum.map(&Map.put(&1, :classes, get_class_count_by_status(&1)))
     end
 
     def hub(conn, %{}) do
       statuses = Repo.all(Status)
-      |> get_class_status_counts
+      |> put_class_status_counts
       render(conn, HubStatusView, "index.json", statuses: statuses)
     end
   end
