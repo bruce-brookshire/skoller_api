60a4270e52b823aa5586b0a8793568e5b95137de
Tyler Witt <tyler@fortyau.com>
2017-11-02 10:05:22 -0500

    Made a route that filters class list on active periods.

--- lib/classnavapi_web/controllers/v1/class_controller.ex
+++ lib/classnavapi_web/controllers/v1/class_controller.ex
@@ -5,6 +5,8 @@ defmodule ClassnavapiWeb.Api.V1.ClassController do
   alias Classnavapi.Repo
   alias ClassnavapiWeb.ClassView
 
+  import Ecto.Query
+
   def create(conn, %{} = params) do
 
     changeset = Class.changeset_insert(%Class{}, params)
@@ -17,6 +19,13 @@ defmodule ClassnavapiWeb.Api.V1.ClassController do
         |> put_status(:unprocessable_entity)
         |> render(ClassnavapiWeb.ChangesetView, "error.json", changeset: changeset)
     end
+  end
+
+  def search(conn, _) do
+    date = Date.utc_today()
+    active_periods = from(period in Classnavapi.ClassPeriod, where: period.start_date <= ^date and period.end_date >= ^date)
+    classes = Repo.all(from class in Class, join: period in subquery(active_periods), on: class.class_period_id == period.id)
+    render(conn, ClassView, "index.json", classes: classes)
   end
 
   def index(conn, _) do
