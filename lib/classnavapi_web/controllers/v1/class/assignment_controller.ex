efc2d92cea5366ee4d0abf0bb8e7f374ea6b77b8
Tyler Witt <tyler@fortyau.com>
2017-11-07 10:10:39 -0600

    correcting assignment weight calc -- in progress

--- lib/classnavapi_web/controllers/v1/class/assignment_controller.ex
+++ lib/classnavapi_web/controllers/v1/class/assignment_controller.ex
@@ -2,6 +2,7 @@ defmodule ClassnavapiWeb.Api.V1.Class.AssignmentController do
   use ClassnavapiWeb, :controller
 
   alias Classnavapi.Class.Assignment
+  alias Classnavapi.Class.Weight
   alias Classnavapi.Repo
   alias ClassnavapiWeb.AssignmentView
 
@@ -22,7 +23,23 @@ defmodule ClassnavapiWeb.Api.V1.Class.AssignmentController do
   end
 
   def index(conn, %{"class_id" => class_id}) do
-    assignments = Repo.all(from a in Assignment, where: a.class_id == ^class_id)
+
+    subquery = (from assign in Assignment)
+    assign_count = subquery
+                    |> join(:inner, [assign], weight in Weight, assign.weight_id == weight.id)
+                    |> where([assign], assign.class_id == ^class_id)
+                    |> group_by([assign, weight], [assign.weight_id, weight.weight])
+                    |> select([assign, weight], %{count: count(assign.id), weight_id: assign.weight_id, weight: weight.weight})
+                    |> Repo.all()
+
+    query = (from assign in Assignment)
+    assignments = query
+                  #|> join(:inner, [assign], ac in subquery(assign_count), ac.weight_id == assign.weight_id)
+                  |> where([assign], assign.class_id == ^class_id)
+                  #|> select([assign, ac], %{assignment: assign, weight: ac})
+                  |> Repo.all()
+    require IEx
+    IEx.pry
     render(conn, AssignmentView, "index.json", assignments: assignments)
   end
 end
