f1cde8e70c57a9a17c34d53f70158a8fe47e54b6
Tyler Witt <tyler@fortyau.com>
2017-11-08 16:34:06 -0600

    Added class issue status name to class show view.

--- lib/classnavapi_web/views/class/issue_view.ex
+++ lib/classnavapi_web/views/class/issue_view.ex
@@ -1,11 +1,16 @@
 defmodule ClassnavapiWeb.Class.IssueView do
   use ClassnavapiWeb, :view
 
+  alias Classnavapi.Repo
+  alias ClassnavapiWeb.Class.Issue.StatusView
+
   def render("issue.json", %{issue: issue}) do
+    issue = issue |> Repo.preload(:class_issue_status)
     %{
       note: issue.note,
       is_completed: issue.is_completed,
-      id: issue.id
+      id: issue.id,
+      issue_status: render_one(issue.class_issue_status, StatusView, "status.json")
     }
   end
 end
