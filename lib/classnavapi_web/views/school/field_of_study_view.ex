b9f90690fb682fbd88eac5aa679feefe64251f79
Tyler Witt <tyler@fortyau.com>
2017-11-09 12:08:51 -0600

    Added field of study routes.

---
+++ lib/classnavapi_web/views/school/field_of_study_view.ex
@@ -0,0 +1,19 @@
+defmodule ClassnavapiWeb.School.FieldOfStudyView do
+  use ClassnavapiWeb, :view
+
+  alias ClassnavapiWeb.School.FieldOfStudyView
+
+  def render("index.json", %{fields: fields}) do
+    render_many(fields, FieldOfStudyView, "field.json", as: :field)
+  end
+
+  def render("show.json", %{field: field}) do
+    render_one(field, FieldOfStudyView, "field.json", as: :field)
+  end
+
+  def render("field.json", %{field: field}) do
+    %{
+      field: field.field
+    }
+  end
+end
