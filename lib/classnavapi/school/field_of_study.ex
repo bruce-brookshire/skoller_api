23c33cc90af40ccd54c353d6fe9e56201519c620
Tyler Witt <tyler@fortyau.com>
2017-11-09 10:51:02 -0600

    Changeset for field of study

--- lib/classnavapi/school/field_of_study.ex
+++ lib/classnavapi/school/field_of_study.ex
@@ -1,8 +1,14 @@
 defmodule Classnavapi.School.FieldOfStudy do
+
+  @moduledoc """
+  
+  Schema and changeset for fields_of_study
+
+  """
+
   use Ecto.Schema
   import Ecto.Changeset
   alias Classnavapi.School.FieldOfStudy
-
 
   schema "fields_of_study" do
     field :field, :string
@@ -11,10 +17,14 @@ defmodule Classnavapi.School.FieldOfStudy do
     timestamps()
   end
 
+  @req_fields [:field, :school_id]
+  @all_fields @req_fields
+
   @doc false
   def changeset(%FieldOfStudy{} = field_of_study, attrs) do
     field_of_study
-    |> cast(attrs, [:field])
-    |> validate_required([:field])
+    |> cast(attrs, @all_fields)
+    |> validate_required(@req_fields)
+    |> foreign_key_constraint(:school_id)
   end
 end
