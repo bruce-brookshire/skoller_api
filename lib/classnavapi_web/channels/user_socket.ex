e808bf166276660a3288eae692c8af98edcced8b
Tyler Witt <tyler@fortyau.com>
2017-11-10 13:03:55 -0600

    added comma

--- lib/classnavapi_web/channels/user_socket.ex
+++ lib/classnavapi_web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule ClassnavapiWeb.UserSocket do
   # channel "room:*", ClassnavapiWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
     timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
