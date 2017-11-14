e2a7dcad1e8660f4bd5082930432cad6ae2558e6
Tyler Witt <tyler@fortyau.com>
2017-11-14 11:29:35 -0600

    Created StudentAssignment table and updated seed.

---
+++ lib/classnavapi/class/student_assignment.ex
@@ -0,0 +1,43 @@
+defmodule Classnavapi.Class.StudentAssignment do
+  
+    @moduledoc """
+    
+    Changeset and schema for student_assignments
+  
+    """
+  
+    use Ecto.Schema
+    import Ecto.Changeset
+    alias Classnavapi.Class.Assignment
+    alias Classnavapi.Class.StudentAssignment
+    alias Classnavapi.Class.StudentClass
+    alias Classnavapi.Class.Weight
+  
+    schema "student_assignments" do
+      field :due, :date
+      field :name, :string
+      field :weight_id, :id
+      field :student_class_id, :id
+      field :assignment_id, :id
+      field :grade, :decimal
+      belongs_to :student_class, StudentClass, define_field: false
+      belongs_to :weight, Weight, define_field: false
+      belongs_to :assignment, Assignment, define_field: false
+  
+      timestamps()
+    end
+  
+    @req_fields [:due, :name, :weight_id, :student_class_id]
+    @opt_fields [:assignment_id, :grade]
+    @all_fields @req_fields ++ @opt_fields
+  
+    @doc false
+    def changeset(%StudentAssignment{} = assignment, attrs) do
+      assignment
+      |> cast(attrs, @all_fields)
+      |> validate_required(@req_fields)
+      |> foreign_key_constraint(:student_class_id)
+      |> foreign_key_constraint(:weight_id)
+    end
+  end
+  
--- priv/repo/migrations/20171026145310_create_assignments.exs
+++ priv/repo/migrations/20171026145310_create_assignments.exs
@@ -13,5 +13,20 @@ defmodule Classnavapi.Repo.Migrations.CreateAssignments do
 
     create index(:assignments, [:class_id])
     create index(:assignments, [:weight_id])
+
+    create table(:student_assignments) do
+      add :name, :string
+      add :due, :date
+      add :weight_id, references(:class_weights, on_delete: :nothing)
+      add :student_class_id, references(:student_classes, on_delete: :nothing)
+      add :assignment_id, references(:assignments, on_delete: :nothing), null: true
+      add :grade, :decimal
+
+      timestamps()
+    end
+
+    create index(:student_assignments, [:student_class_id])
+    create index(:student_assignments, [:weight_id])
+    create index(:student_assignments, [:assignment_id])
   end
 end
