70988a4599783a319bba171e4638411da1b33b44
Tyler Witt <tyler@fortyau.com>
2017-11-28 16:01:24 -0600

    Added role security to classes

--- lib/classnavapi_web/controllers/v1/class_controller.ex
+++ lib/classnavapi_web/controllers/v1/class_controller.ex
@@ -14,8 +14,15 @@ defmodule ClassnavapiWeb.Api.V1.ClassController do
   alias ClassnavapiWeb.Helpers.StatusHelper
 
   import Ecto.Query
-
+  import ClassnavapiWeb.Helpers.AuthPlug
+  
+  @student_role 100
+  @admin_role 200
+  @syllabus_worker_role 300
+  @change_req_role 400
   @default_grade_scale "A,90|B,80|C,70|D,60"
+  
+  plug :verify_role, %{roles: [@student_role, @admin_role, @syllabus_worker_role, @change_req_role]}
 
   @doc """
    Confirms that a `Classnavapi.Class` is ready to change `Classnavapi.Class.Status` 
