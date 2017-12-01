74fca7ae5f40e4add6919a993b937f1d69c8aa0a
Tyler Witt <tyler@fortyau.com>
2017-12-01 09:12:45 -0600

    Added color to student class as an optional field.

--- lib/classnavapi/class/student_class.ex
+++ lib/classnavapi/class/student_class.ex
@@ -14,6 +14,7 @@ defmodule Classnavapi.Class.StudentClass do
   schema "student_classes" do
     field :student_id, :id
     field :class_id, :id
+    field :color, :string
     belongs_to :class, Classnavapi.Class, define_field: false
     belongs_to :student, Classnavapi.Student, define_field: false
     has_many :student_assignments, StudentAssignment
@@ -22,7 +23,8 @@ defmodule Classnavapi.Class.StudentClass do
   end
 
   @req_fields [:student_id, :class_id]
-  @all_fields @req_fields
+  @opt_fields [:color]
+  @all_fields @req_fields ++ @opt_fields
 
   @doc false
   def changeset(%StudentClass{} = student_class, attrs) do
