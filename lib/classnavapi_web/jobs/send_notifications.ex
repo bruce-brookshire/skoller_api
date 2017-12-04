e4818086e197b6a68439318a02b54cdd4febd0fe
Tyler Witt <tyler@fortyau.com>
2017-12-04 14:45:21 -0600

    Modified notification logic to include all days between days prior and due date.

--- lib/classnavapi_web/jobs/send_notifications.ex
+++ lib/classnavapi_web/jobs/send_notifications.ex
@@ -8,10 +8,12 @@ defmodule ClassnavapiWeb.Jobs.SendNotifications do
   import Ecto.Query
 
   def run(time) do
-    assignments = time |> query
+    time 
+    |> assignment_query() 
+    |> send_notifications()
   end
 
-  defp query(time) do
+  defp assignment_query(time) do
     {:ok, time} = Time.new(time.hour, time.minute, 0, 0)
     now = Date.utc_today()
 
@@ -20,8 +22,12 @@ defmodule ClassnavapiWeb.Jobs.SendNotifications do
     |> join(:inner, [student, sclass], sassign in StudentAssignment, sassign.student_class_id == sclass.id and sassign.is_notifications == true)
     |> where([student], student.notification_time == ^time)
     |> where([student], student.is_notifications == true)
-    |> where([student, sclass, sassign], sassign.due == ^now or sassign.due == date_add(^now, student.notification_days_notice, "day"))
-    |> select([student, sclass, sassign], %{student: student, class: sclass, assign: sassign})
+    |> where([student, sclass, sassign], sassign.due >= ^now and sassign.due <= date_add(^now, student.notification_days_notice, "day"))
+    |> select([student, sclass, sassign], %{student: student, class: sclass, assign_name: sassign.name, assign_due: sassign.due})
     |> Repo.all()
+  end
+
+  defp send_notifications(assignments) do
+    
   end
 end
