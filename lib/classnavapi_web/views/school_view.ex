6b81b1b91fc2e3f459508213005de69a0767eadf
Tyler Witt <tyler@fortyau.com>
2017-12-05 10:52:03 -0600

    Added periods and schools to user view.

--- lib/classnavapi_web/views/school_view.ex
+++ lib/classnavapi_web/views/school_view.ex
@@ -3,6 +3,7 @@ defmodule ClassnavapiWeb.SchoolView do
 
   alias ClassnavapiWeb.SchoolView
   alias ClassnavapiWeb.School.EmailDomainView
+  alias ClassnavapiWeb.PeriodView
   alias Classnavapi.Repo
 
   def render("index.json", %{schools: schools}) do
@@ -14,11 +15,19 @@ defmodule ClassnavapiWeb.SchoolView do
   end
 
   def render("school.json", %{school: school}) do
-    school = school |> Repo.preload(:email_domains)
+    school = school |> Repo.preload([:email_domains, :class_periods])
     %{
       id: school.id,
       name: school.name,
+      periods: active_periods(school.class_periods),
       email_domains: render_many(school.email_domains, EmailDomainView, "email_domain.json")
     }
+  end
+
+  defp active_periods(class_periods) do
+    today = Date.utc_today()
+    class_periods 
+    |> Enum.filter(&Date.compare(&1.start_date, today) in [:lt, :eq] and Date.compare(&1.end_date, today) in [:gt, :eq])
+    |> render_many(PeriodView, "period.json")
   end
 end
