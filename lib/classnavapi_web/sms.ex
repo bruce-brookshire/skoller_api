8c894e955abf36a7f1883e38ee0b8b98cacca7fc
Tyler Witt <tyler@fortyau.com>
2017-12-05 10:10:52 -0600

    Test phone credentials added.

--- lib/classnavapi_web/sms.ex
+++ lib/classnavapi_web/sms.ex
@@ -6,15 +6,24 @@ defmodule ClassnavapiWeb.Sms do
   alias ExTwilio.Message
 
   @from_phone "+16152099126"
+  @test_phone "+15005550006"
 
   @verification_msg1 "Your Skoller verification code is "
   @verification_msg2 ". #SkollerAtMe"
 
   def verify_phone(phone, code) do
-    Message.create(
-      to: "+1" <> phone,
-      from: @from_phone,
-      body: @verification_msg1 <> code <> @verification_msg2
-    )
+    case Mix.env do
+      :prod -> Message.create(
+                to: "+1" <> phone,
+                from: @from_phone,
+                body: @verification_msg1 <> code <> @verification_msg2
+              )
+      _ -> Message.create(
+              to: "+1" <> phone,
+              from: @test_phone,
+              body: @verification_msg1 <> code <> @verification_msg2
+            )
+    end
+    
   end
 end
