403db91c48a54ba552c9a485bf211c51bdc371fc
Tyler Witt <tyler@fortyau.com>
2017-12-07 15:42:29 -0600

    Assignment locking

--- lib/classnavapi_web/helpers/lock_plug.ex
+++ lib/classnavapi_web/helpers/lock_plug.ex
@@ -9,12 +9,14 @@ defmodule ClassnavapiWeb.Helpers.LockPlug do
   alias Classnavapi.Repo
   alias Classnavapi.Class.Lock
   alias Classnavapi.Class.Weight
+  alias Classnavapi.Class.Assignment
 
   import Plug.Conn
 
   @admin_role 200
 
   @weight_lock 100
+  @assignment_lock 200
 
   def check_lock(conn, params) do
     case conn |> is_admin() do
@@ -46,8 +48,33 @@ defmodule ClassnavapiWeb.Helpers.LockPlug do
     end
   end
 
+  defp get_lock(%{assigns: %{user: user}} = conn, %{type: :assignment, using: :id}) do
+    case conn.params |> check_using(:assignment, :id) do
+      true ->
+        assign = Repo.get!(Assignment, conn.params["id"])
+        case Repo.get_by(Lock, class_id: assign.class_id, class_lock_section_id: @assignment_lock, user_id: user.id, is_completed: false) do
+          nil -> conn |> unauth
+          _ -> conn
+        end
+      false -> conn
+    end
+  end
+
+  defp get_lock(%{assigns: %{user: user}} = conn, %{type: :assignment, using: :class_id}) do
+    case conn.params |> check_using(:assignment, :class_id) do
+      true ->
+        case Repo.get_by(Lock, class_id: conn.params["class_id"], class_lock_section_id: @assignment_lock, user_id: user.id, is_completed: false) do
+          nil -> conn |> unauth
+          _ -> conn
+        end
+      false -> conn
+    end
+  end
+
   defp check_using(%{"id" => _}, :weight, :id), do: true
   defp check_using(%{"class_id" => _}, :weight, :class_id), do: true
+  defp check_using(%{"id" => _}, :assignment, :id), do: true
+  defp check_using(%{"class_id" => _}, :assignment, :class_id), do: true
   defp check_using(_params, _, _), do: false
 
   defp is_admin(conn) do
