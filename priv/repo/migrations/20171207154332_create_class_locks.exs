b043c64cbfa2f7971fd628e40defaaa009d5c5fb
Tyler Witt <tyler@fortyau.com>
2017-12-07 09:55:37 -0600

    Added lock tables and seed for lock types.

---
+++ priv/repo/migrations/20171207154332_create_class_locks.exs
@@ -0,0 +1,19 @@
+defmodule Classnavapi.Repo.Migrations.CreateClassLocks do
+  use Ecto.Migration
+
+  def change do
+    create table(:class_locks) do
+      add :is_completed, :boolean, default: false, null: false
+      add :class_lock_section_id, references(:class_lock_sections, on_delete: :nothing)
+      add :class_id, references(:classes, on_delete: :nothing)
+      add :user_id, references(:users, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create index(:class_locks, [:class_lock_section_id])
+    create index(:class_locks, [:class_id])
+    create index(:class_locks, [:user_id])
+    create unique_index(:class_locks, [:class_id, :class_lock_section_id])
+  end
+end
