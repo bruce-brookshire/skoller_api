7842e7b55463941535a57f8417394316ba0c43cf
Tyler Witt <tyler@fortyau.com>
2017-12-08 14:40:17 -0600

    Exclude non-enrollable schools from no auth school list.

--- lib/classnavapi_web/controllers/v1/school_controller.ex
+++ lib/classnavapi_web/controllers/v1/school_controller.ex
@@ -5,8 +5,12 @@ defmodule ClassnavapiWeb.Api.V1.SchoolController do
   alias Classnavapi.Repo
   alias ClassnavapiWeb.SchoolView
 
+  import Ecto.Query
+
   def index(conn, _params) do
-    schools = Repo.all(School)
+    schools = from(school in School)
+              |> where([school], school.is_active_enrollment == true) 
+              |> Repo.all()
     render(conn, SchoolView, "index.json", schools: schools)
   end
 end
