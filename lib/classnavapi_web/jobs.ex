c38e38d270e3c4033e3c38055e4220fa82fbbaad
Tyler Witt <tyler@fortyau.com>
2017-12-13 15:13:26 -0600

    Clear locks job

--- lib/classnavapi_web/jobs.ex
+++ lib/classnavapi_web/jobs.ex
@@ -3,7 +3,12 @@ defmodule ClassnavapiWeb.Jobs do
   alias ClassnavapiWeb.Jobs
 
   def run() do
-    Time.utc_now()
+    now = Time.utc_now()
+
+    now
     |> Jobs.SendNotifications.run()
+
+    now
+    |> Jobs.ClearLocks.run()
   end
 end
