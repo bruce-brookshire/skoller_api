ff519f9fb63272b5e72641770b1fcf22c57abec4
Tyler Witt <tyler@fortyau.com>
2017-12-18 11:40:44 -0600

    Changed dots to underscores

--- lib/classnavapi_web/controllers/v1/school/class_controller.ex
+++ lib/classnavapi_web/controllers/v1/school/class_controller.ex
@@ -47,39 +47,39 @@ defmodule ClassnavapiWeb.Api.V1.School.ClassController do
     |> number_filter(params)
   end
 
-  defp prof_filter(dynamic, %{"professor.name" => filter, "or" => "true"}) do
+  defp prof_filter(dynamic, %{"professor_name" => filter, "or" => "true"}) do
     prof_filter = "%" <> filter <> "%"
     dynamic([class, period, prof], ilike(prof.name_last, ^prof_filter) or ^dynamic)
   end
-  defp prof_filter(dynamic, %{"professor.name" => filter}) do
+  defp prof_filter(dynamic, %{"professor_name" => filter}) do
     prof_filter = "%" <> filter <> "%"
     dynamic([class, period, prof], ilike(prof.name_last, ^prof_filter) and ^dynamic)
   end
   defp prof_filter(dynamic, _), do: dynamic
 
-  defp prof_id_filter(dynamic, %{"professor.id" => filter, "or" => "true"}) do
+  defp prof_id_filter(dynamic, %{"professor_id" => filter, "or" => "true"}) do
     dynamic([class, period, prof], prof.id == ^filter or ^dynamic)
   end
-  defp prof_id_filter(dynamic, %{"professor.id" => filter}) do
+  defp prof_id_filter(dynamic, %{"professor_id" => filter}) do
     dynamic([class, period, prof], prof.id == ^filter and ^dynamic)
   end
   defp prof_id_filter(dynamic, _), do: dynamic
 
-  defp name_filter(dynamic, %{"class.name" => filter, "or" => "true"}) do
+  defp name_filter(dynamic, %{"class_name" => filter, "or" => "true"}) do
     name_filter = "%" <> filter <> "%"
     dynamic([class, period, prof], ilike(class.name, ^name_filter) or ^dynamic)
   end
-  defp name_filter(dynamic, %{"class.name" => filter}) do
+  defp name_filter(dynamic, %{"class_name" => filter}) do
     name_filter = "%" <> filter <> "%"
     dynamic([class, period, prof], ilike(class.name, ^name_filter) and ^dynamic)
   end
   defp name_filter(dynamic, _), do: dynamic
 
-  defp number_filter(dynamic, %{"class.number" => filter, "or" => "true"}) do
+  defp number_filter(dynamic, %{"class_number" => filter, "or" => "true"}) do
     number_filter = "%" <> filter <> "%"
     dynamic([class, period, prof], ilike(class.number, ^number_filter) or ^dynamic)
   end
-  defp number_filter(dynamic, %{"class.number" => filter}) do
+  defp number_filter(dynamic, %{"class_number" => filter}) do
     number_filter = "%" <> filter <> "%"
     dynamic([class, period, prof], ilike(class.number, ^number_filter) and ^dynamic)
   end
