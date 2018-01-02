0a7c64b40640ceab21327c374cea562724271992
Tyler Witt <tyler@fortyau.com>
2018-01-02 16:19:28 -0600

    Convert image formats to pdf

--- lib/classnavapi_web/doc_upload.ex
+++ lib/classnavapi_web/doc_upload.ex
@@ -27,8 +27,16 @@ defmodule Classnavapi.DocUpload do
     "uploads/class/docs/"
   end
 
+  def transform(:original, {file, scope}) do
+    case String.ends_with?(file.file_name, [".jpg", ".png", ".jpeg"]) do
+      true -> 
+        {:convert, "-gravity center", :pdf}
+      false -> :noaction
+    end
+  end
+
   def s3_object_headers(_version, {file, _scope}) do
-    [content_type: Plug.MIME.path(file.file_name)] # for "image.png", would produce: "image/png"
+    [content_type: MIME.from_path(file.file_name)] # for "image.png", would produce: "image/png"
   end
 
   # Specify custom headers for s3 objects
