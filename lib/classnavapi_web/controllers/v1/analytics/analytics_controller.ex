8fa78ee8d922cccff37018ddc35f1b69813a8a8d
Tyler Witt <tyler@fortyau.com>
2018-01-31 16:15:51 -0600

    Added date filters to analytics.

--- lib/classnavapi_web/controllers/v1/analytics/analytics_controller.ex
+++ lib/classnavapi_web/controllers/v1/analytics/analytics_controller.ex
@@ -14,32 +14,40 @@ defmodule ClassnavapiWeb.Api.V1.Analytics.AnalyticsController do
   plug :verify_role, %{role: @admin_role}
 
   def index(conn, params) do
+    dates = Map.new 
+            |> Map.put(:date_start, Date.from_iso8601!(params["date_start"]))
+            |> Map.put(:date_end, Date.from_iso8601!(params["date_end"]))
     syllabi = Map.new()
-    |> Map.put(:class_count, class_count(params))
-    |> Map.put(:enrollment, enrollment_count(params))
+    |> Map.put(:class_count, class_count(dates, params))
+    |> Map.put(:enrollment, enrollment_count(dates, params))
   end
 
-  defp enrollment_count(%{"school_id" => school_id}) do
+  defp enrollment_count(dates, %{"school_id" => school_id}) do
     from(sc in StudentClass)
     |> join(:inner, [sc], c in Class, c.id == sc.class_id)
     |> join(:inner, [sc, c], p in ClassPeriod, c.class_period_id == p.id)
     |> where([sc, c, p], p.school_id == ^school_id)
+    |> where([sc], fragment("?::date", sc.inserted_at) >= ^dates.date_start and fragment("?::date", sc.inserted_at) <= ^dates.date_end)
     |> select([sc, c, p], count(sc.class_id, :distinct))
     |> Repo.one
   end
-  defp enrollment_count(_params) do
+  defp enrollment_count(dates, _params) do
     from(sc in StudentClass)
+    |> where([sc], fragment("?::date", sc.inserted_at) >= ^dates.date_start and fragment("?::date", sc.inserted_at) <= ^dates.date_end)
     |> select([sc], count(sc.class_id, :distinct))
     |> Repo.one
   end
 
-  defp class_count(%{"school_id" => school_id}) do
+  defp class_count(dates, %{"school_id" => school_id}) do
     from(c in Class)
     |> join(:inner, [c], p in ClassPeriod, c.class_period_id == p.id)
     |> where([c, p], p.school_id == ^school_id)
+    |> where([c], fragment("?::date", c.inserted_at) >= ^dates.date_start and fragment("?::date", c.inserted_at) <= ^dates.date_end)
     |> Repo.aggregate(:count, :id)
   end
-  defp class_count(_params) do
-    Repo.aggregate(Class, :count, :id)
+  defp class_count(dates, _params) do
+    from(c in Class)
+    |> where([c], fragment("?::date", c.inserted_at) >= ^dates.date_start and fragment("?::date", c.inserted_at) <= ^dates.date_end)
+    |> Repo.aggregate(:count, :id)
   end
 end
