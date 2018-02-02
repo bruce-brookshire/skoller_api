f05985e995944eed88bb20b90b63ae26a6540351
Tyler Witt <tyler@fortyau.com>
2018-02-02 14:58:13 -0600

    Added chat posts to chat channel.

--- lib/classnavapi_web/channels/chat_channel.ex
+++ lib/classnavapi_web/channels/chat_channel.ex
@@ -1,15 +1,26 @@
 defmodule ClassnavapiWeb.ChatChannel do
   use Phoenix.Channel
 
-  def join("room:lobby", _message, socket) do
-    {:ok, socket}
-  end
-  def join("room:" <> _private_room_id, _params, _socket) do
-    {:error, %{reason: "unauthorized"}}
+  alias Classnavapi.Repo
+  alias Classnavapi.Class.StudentClass
+  alias Classnavapi.Chat.Post
+
+  def join("chat:" <> class_id, _params, socket) do
+    case Repo.get_by(StudentClass, student_id: socket.assigns.user.student.id, class_id: class_id, is_dropped: false) do
+      nil -> {:error, %{reason: "unauthorized"}}
+      _ -> {:ok, socket}
+    end
   end
 
-  def handle_in("new_msg", %{"body" => body}, socket) do
-    broadcast! socket, "new_msg", %{body: body}
-    {:noreply, socket}
+  def handle_in("post", %{"body" => body}, socket) do
+    "chat:" <> class_id = socket.topic
+    changeset = Post.changeset(%Post{}, %{class_id: class_id, student_id: socket.assigns.user.student.id, post: body})
+    case Repo.insert(changeset) do
+      {:ok, _post} ->
+        broadcast! socket, "post", %{body: body}
+        {:noreply, socket}
+      {:error, _changeset} ->
+        {:error, %{reason: "Failed to create post"}}
+    end
   end
 end
