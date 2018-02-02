8a6f3ff842d4ef9952b072c882655d889de2de77
Tyler Witt <tyler@fortyau.com>
2018-02-02 15:49:03 -0600

    Handle comments and replies in chat.

--- lib/classnavapi_web/channels/chat_channel.ex
+++ lib/classnavapi_web/channels/chat_channel.ex
@@ -4,6 +4,8 @@ defmodule ClassnavapiWeb.ChatChannel do
   alias Classnavapi.Repo
   alias Classnavapi.Class.StudentClass
   alias Classnavapi.Chat.Post
+  alias Classnavapi.Chat.Comment
+  alias Classnavapi.Chat.Reply
 
   def join("chat:" <> class_id, _params, socket) do
     case Repo.get_by(StudentClass, student_id: socket.assigns.user.student.id, class_id: class_id, is_dropped: false) do
@@ -20,7 +22,29 @@ defmodule ClassnavapiWeb.ChatChannel do
         broadcast! socket, "post", %{body: body}
         {:noreply, socket}
       {:error, _changeset} ->
-        {:error, %{reason: "Failed to create post"}}
+        {:reply, %{error: "Failed to create post"}}
+    end
+  end
+
+  def handle_in("comment", %{"body" => body, "post_id" => post_id}, socket) do
+    changeset = Comment.changeset(%Comment{}, %{chat_post_id: post_id, student_id: socket.assigns.user.student.id, comment: body})
+    case Repo.insert(changeset) do
+      {:ok, _comment} ->
+        broadcast! socket, "comment", %{body: body, post_id: post_id}
+        {:noreply, socket}
+      {:error, _changeset} ->
+        {:reply, %{error: "Failed to add comment"}}
+    end
+  end
+
+  def handle_in("reply", %{"body" => body, "comment_id" => comment_id}, socket) do
+    changeset = Reply.changeset(%Reply{}, %{chat_comment_id: comment_id, student_id: socket.assigns.user.student.id, reply: body})
+    case Repo.insert(changeset) do
+      {:ok, _reply} ->
+        broadcast! socket, "reply", %{body: body, comment_id: comment_id}
+        {:noreply, socket}
+      {:error, _changeset} ->
+        {:reply, %{error: "Failed to reply"}}
     end
   end
 end
