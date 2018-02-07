69040dfbdca5fc12747f460576415358cd031270
Tyler Witt <tyler@fortyau.com>
2018-02-07 13:13:13 -0600

    All student views for chat posts include color.

--- lib/classnavapi_web/views/class/chat_post_view.ex
+++ lib/classnavapi_web/views/class/chat_post_view.ex
@@ -8,6 +8,10 @@ defmodule ClassnavapiWeb.Class.ChatPostView do
   alias ClassnavapiWeb.Helpers.ChatHelper
   alias Classnavapi.Chat.Post.Star
 
+  def render("index.json", %{chat_posts: %{chat_posts: chat_posts, color: color}, current_student_id: current_student_id}) do
+    render_many(chat_posts, ChatPostView, "chat_post.json", %{current_student_id: current_student_id, color: color})
+  end
+
   def render("index.json", %{chat_posts: chat_posts, current_student_id: current_student_id}) do
     render_many(chat_posts, ChatPostView, "chat_post.json", %{current_student_id: current_student_id})
   end
@@ -24,33 +28,12 @@ defmodule ClassnavapiWeb.Class.ChatPostView do
     render_one(chat_post, ChatPostView, "chat_post_detail.json")
   end
 
-  def render("chat_post.json", %{chat_post: %{chat_post: chat_post, color: color}, current_student_id: student_id}) do
-    chat_post = chat_post |> Repo.preload([:student, :chat_comments, :likes, :class])
-    %{
-      post: chat_post.post,
-      student: render_one(chat_post.student, ClassnavapiWeb.StudentView, "student-short.json"),
-      id: chat_post.id,
-      comments: render_many(chat_post.chat_comments, ChatCommentView, "chat_comment.json"),
-      likes: render_many(chat_post.likes, LikeView, "like.json"),
-      is_liked: chat_post.likes |> ChatHelper.is_liked(student_id),
-      is_starred: chat_post |> is_starred(student_id),
-      class: render_one(chat_post.class, ClassnavapiWeb.ClassView, "class_short.json"),
-      color: color
-    }
+  def render("chat_post.json", %{chat_post: chat_post, current_student_id: student_id, color: color}) do
+    student_chat_post(chat_post, color, student_id)
   end
 
-  def render("chat_post.json", %{chat_post: chat_post, current_student_id: student_id}) do
-    chat_post = chat_post |> Repo.preload([:student, :chat_comments, :likes, :class])
-    %{
-      post: chat_post.post,
-      student: render_one(chat_post.student, ClassnavapiWeb.StudentView, "student-short.json"),
-      id: chat_post.id,
-      comments: render_many(chat_post.chat_comments, ChatCommentView, "chat_comment.json"),
-      likes: render_many(chat_post.likes, LikeView, "like.json"),
-      is_liked: chat_post.likes |> ChatHelper.is_liked(student_id),
-      is_starred: chat_post |> is_starred(student_id),
-      class: render_one(chat_post.class, ClassnavapiWeb.ClassView, "class_short.json")
-    }
+  def render("chat_post.json", %{chat_post: %{chat_post: chat_post, color: color}, current_student_id: student_id}) do
+    student_chat_post(chat_post, color, student_id)
   end
 
   def render("chat_post.json", %{chat_post: chat_post}) do
@@ -79,20 +62,6 @@ defmodule ClassnavapiWeb.Class.ChatPostView do
     }
   end
 
-  def render("chat_post_detail.json", %{chat_post: chat_post, current_student_id: student_id}) do
-    chat_post = chat_post |> Repo.preload([:student, :chat_comments, :likes, :class])
-    %{
-      post: chat_post.post,
-      student: render_one(chat_post.student, ClassnavapiWeb.StudentView, "student-short.json"),
-      id: chat_post.id,
-      comments: render_many(chat_post.chat_comments, ChatCommentView, "chat_comment_detail.json", %{current_student_id: student_id}),
-      likes: render_many(chat_post.likes, LikeView, "like.json"),
-      is_liked: chat_post.likes |> ChatHelper.is_liked(student_id),
-      is_starred: chat_post |> is_starred(student_id),
-      class: render_one(chat_post.class, ClassnavapiWeb.ClassView, "class_short.json"),
-    }
-  end
-
   def render("chat_post_detail.json", %{chat_post: chat_post}) do
     chat_post = chat_post |> Repo.preload([:student, :chat_comments, :likes])
     %{
@@ -101,6 +70,21 @@ defmodule ClassnavapiWeb.Class.ChatPostView do
       id: chat_post.id,
       comments: render_many(chat_post.chat_comments, ChatCommentView, "chat_comment_detail.json"),
       likes: render_many(chat_post.likes, LikeView, "like.json")
+    }
+  end
+
+  defp student_chat_post(chat_post, color, student_id) do
+    chat_post = chat_post |> Repo.preload([:student, :chat_comments, :likes, :class])
+    %{
+      post: chat_post.post,
+      student: render_one(chat_post.student, ClassnavapiWeb.StudentView, "student-short.json"),
+      id: chat_post.id,
+      comments: render_many(chat_post.chat_comments, ChatCommentView, "chat_comment.json"),
+      likes: render_many(chat_post.likes, LikeView, "like.json"),
+      is_liked: chat_post.likes |> ChatHelper.is_liked(student_id),
+      is_starred: chat_post |> is_starred(student_id),
+      class: render_one(chat_post.class, ClassnavapiWeb.ClassView, "class_short.json"),
+      color: color
     }
   end
 
