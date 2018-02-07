4fcb10918faf4ec7e2a48f0f0f4222d8e4cc0739
Tyler Witt <tyler@fortyau.com>
2018-02-07 15:00:31 -0600

    Added enrollment to student chat query. bug fixed most recent sort.

--- lib/classnavapi_web/controllers/v1/student/chat_controller.ex
+++ lib/classnavapi_web/controllers/v1/student/chat_controller.ex
@@ -12,7 +12,7 @@ defmodule ClassnavapiWeb.Api.V1.Student.ChatController do
   @student_role 100
 
   # @sort_hot 100
-  @sort_recent 200
+  @sort_recent "200"
   # @sort_top_day 300
   # @sort_top_week 400
   # @sort_top_period 500
@@ -23,12 +23,22 @@ defmodule ClassnavapiWeb.Api.V1.Student.ChatController do
   def chat(conn, %{"student_id" => student_id} = params) do
     posts = from(p in Post)
     |> join(:inner, [p], sc in StudentClass, sc.class_id == p.class_id)
+    |> join(:inner, [p, sc], enroll in subquery(enrollment_subquery(student_id)), enroll.class_id == sc.class_id)
     |> where([p, sc], sc.student_id == ^student_id and sc.is_dropped == false)
-    |> select([p, sc], %{chat_post: p, color: sc.color})
+    |> select([p, sc, enroll], %{chat_post: p, color: sc.color, enroll: enroll.count})
     |> order_by_params(params)
     |> Repo.all()
 
     render(conn, ChatPostView, "index.json", %{chat_posts: posts, current_student_id: student_id})
+  end
+
+  defp enrollment_subquery(student_id) do
+    from(sc in StudentClass)
+    |> join(:inner, [sc], enroll in StudentClass, sc.class_id == enroll.class_id)
+    |> where([sc], sc.student_id == ^student_id and sc.is_dropped == false)
+    |> where([sc, enroll], enroll.is_dropped == false)
+    |> group_by([sc, enroll], enroll.class_id)
+    |> select([sc, enroll], %{class_id: enroll.class_id, count: count(enroll.id)})
   end
 
   defp order_by_params(query, %{"sort" => @sort_recent}) do
