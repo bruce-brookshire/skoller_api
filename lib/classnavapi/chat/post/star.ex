99f56582da808536933e0258f7214d3f81b3f60f
Tyler Witt <tyler@fortyau.com>
2018-02-08 10:06:48 -0600

    Make stars contain an is_read boolean

--- lib/classnavapi/chat/post/star.ex
+++ lib/classnavapi/chat/post/star.ex
@@ -7,12 +7,13 @@ defmodule Classnavapi.Chat.Post.Star do
   schema "chat_post_stars" do
     field :chat_post_id, :id
     field :student_id, :id
+    field :is_read, :boolean, default: false
     belongs_to :chat_post, Classnavapi.Chat.Post, define_field: false
 
     timestamps()
   end
 
-  @req_fields [:chat_post_id, :student_id]
+  @req_fields [:chat_post_id, :student_id, :is_read]
   @all_fields @req_fields
 
   @doc false
---
+++ priv/repo/migrations/20180208160216_make_inbox_readable.exs
@@ -0,0 +1,13 @@
+defmodule Classnavapi.Repo.Migrations.MakeInboxReadable do
+  use Ecto.Migration
+
+  def change do
+    alter table(:chat_post_stars) do
+      add :is_read, :boolean, default: false, null: false
+    end
+
+    alter table(:chat_comment_stars) do
+      add :is_read, :boolean, default: false, null: false
+    end
+  end
+end
