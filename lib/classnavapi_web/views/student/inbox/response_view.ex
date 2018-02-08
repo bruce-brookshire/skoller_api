704e5b5bc150f9a6015303b733dcac5788e91d70
Tyler Witt <tyler@fortyau.com>
2018-02-08 17:19:10 -0600

    Get student obj that posted inbox update.

--- lib/classnavapi_web/views/student/inbox/response_view.ex
+++ lib/classnavapi_web/views/student/inbox/response_view.ex
@@ -2,15 +2,29 @@ defmodule ClassnavapiWeb.Student.Inbox.ResponseView do
   use ClassnavapiWeb, :view
 
   alias ClassnavapiWeb.Student.Inbox.ResponseView
+  alias Classnavapi.Repo
+  alias Classnavapi.Chat.Reply
+  alias Classnavapi.Chat.Comment
 
   def render("show.json", %{response: response}) do
     render_one(response, ResponseView, "response.json")
   end
 
-  def render("response.json", %{response: response}) do
+  def render("response.json", %{response: %{is_reply: false} = response}) do
+    comment = Repo.get!(Comment, response.id) |> Repo.preload(:student)
     %{
       response: response.response,
-      is_reply: response.is_reply
+      is_reply: response.is_reply,
+      student: render_one(comment.student, ClassnavapiWeb.StudentView, "student-short.json")
+    }
+  end
+
+  def render("response.json", %{response: %{is_reply: true} = response}) do
+    reply = Repo.get!(Reply, response.id) |> Repo.preload(:student)
+    %{
+      response: response.response,
+      is_reply: response.is_reply,
+      student: render_one(reply.student, ClassnavapiWeb.StudentView, "student-short.json")
     }
   end
 end
