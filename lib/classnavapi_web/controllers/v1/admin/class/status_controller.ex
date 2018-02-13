33d8b020c445ea66572e94ed24667057ca593791
Tyler Witt <tyler@fortyau.com>
2018-02-13 09:35:09 -0600

    Added needs syllabus email.

--- lib/classnavapi_web/controllers/v1/admin/class/status_controller.ex
+++ lib/classnavapi_web/controllers/v1/admin/class/status_controller.ex
@@ -21,6 +21,7 @@ defmodule ClassnavapiWeb.Api.V1.Admin.Class.StatusController do
     @help_role 500
 
     @new_class_status 100
+    @syllabus_status 200
     @assignment_status 400
     @review_status 500
     @help_status 600
@@ -32,13 +33,25 @@ defmodule ClassnavapiWeb.Api.V1.Admin.Class.StatusController do
 
     @from_email "support@skoller.co"
     @deny_subj " has been denied"
+    @syllabus_subj "Wrong Syllabus?"
+    @syllabus_greeting "Hi there,"
     @greeting "Hi "
     @comma ","
     @ending "Thank you"
+    @divider "--"
+    @skoller "The Skoller Team"
 
     @your_request "Your request to create a new class, "
     @has_been_denied " has been denied. "
     @reason "The reason is because that class already exists at your school or the request is illegitimate. Please contact us at support@skoller.co for further assistance."
+
+    @wrong_syllabus_submitted "Our team hass noticed that the wrong syllabus was submitted for "
+    @no_biggie ". No biggie, we all goof up every now and then!"
+    @we_deleted_it " We deleted the wrong syllabus."
+    @you_should " You should now be able to "
+    @sign_in "sign in"
+    @upload_correct_syllabus " and upload the correct syllabus when you're ready!"
+    @syllabus_ending "We hope you and your classmates have a great semester!"
     
     plug :verify_role, %{roles: [@admin_role, @help_role]}
 
@@ -88,12 +101,30 @@ defmodule ClassnavapiWeb.Api.V1.Admin.Class.StatusController do
             Task.start(NotificationHelper, :send_class_complete_notification, [class])
           end
           render(conn, ClassView, "show.json", class: class)
+        {:ok, %{class: %{class_status_id: @syllabus_status} = class}} ->
+          from(u in User)
+          |> join(:inner, [u], sc in StudentClass, sc.student_id == u.student_id)
+          |> where([u, sc], sc.class_id == ^class.id and sc.is_dropped == false)
+          |> Repo.all()
+          |> Enum.each(&send_need_syllabus_email(&1, class))
+          render(conn, ClassView, "show.json", class: class)
         {:ok, %{class: class}} ->
           render(conn, ClassView, "show.json", class: class)
         {:error, _, failed_value, _} ->
           conn
           |> RepoHelper.multi_error(failed_value)
       end
+    end
+
+    defp send_need_syllabus_email(user, class) do
+      user = user |> Repo.preload(:student)
+      new_email()
+      |> to(user.email)
+      |> Bamboo.Email.from(@from_email)
+      |> subject(@syllabus_subj)
+      |> html_body(syllabus_html_body(class))
+      |> text_body(syllabus_text_body(class))
+      |> Mailer.deliver_later
     end
 
     defp deny_class(conn, %Class{class_status_id: @new_class_status} = class) do
@@ -140,6 +171,29 @@ defmodule ClassnavapiWeb.Api.V1.Admin.Class.StatusController do
       @your_request <> class.name <> @has_been_denied <> @reason <> "\n" <>
       "\n" <>
       @ending
+    end
+
+    defp syllabus_html_body(class) do
+      "<p>" <> @syllabus_greeting <> "<br />" <>
+      "<br />" <>
+      @wrong_syllabus_submitted <> class.name <> @no_biggie <> @we_deleted_it <> @you_should <>
+      "<a href=" <> to_string(System.get_env("WEB_URL")) <> ">" <> @sign_in <> "</a>" <>
+      @upload_correct_syllabus <> "<br />" <>
+      "<br />" <>
+      @syllabus_ending <> "</p>" <> 
+      "<h3>" <> @divider <> "</h3>" <> 
+      "<h3>" <> @skoller <> "</h3>"
+    end
+
+    defp syllabus_text_body(class) do
+      @syllabus_greeting <> "\n" <>
+      "\n" <>
+      @wrong_syllabus_submitted <> class.name <> @no_biggie <> @we_deleted_it <> @you_should <> @sign_in <> @upload_correct_syllabus <> "\n" <>
+      "\n" <>
+      @syllabus_ending <> "\n" <>
+      "\n" <>
+      @divider <> "\n" <>
+      @skoller
     end
 
     defp reset_locks(_class, %{is_complete: true}), do: {:ok, nil}
