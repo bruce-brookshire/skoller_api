ead59b717084ac7f27db8dceb751071833288f7a
Tyler Witt <tyler@fortyau.com>
2018-02-19 14:50:42 -0600

    Loggin

--- lib/classnavapi_web/helpers/chat_helper.ex
+++ lib/classnavapi_web/helpers/chat_helper.ex
@@ -1,6 +1,9 @@
 defmodule ClassnavapiWeb.Helpers.ChatHelper do
   def is_liked([]), do: false
   def is_liked(enum, student_id) do
+    require Logger
+    Logger.info(inspect(enum))
+    Logger.info(student_id)
     enum |> Enum.any?(& &1.student_id == student_id)
   end
 end
