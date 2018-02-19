8f7a3183417297f7feea4fa5349cb5e48a043fa5
Tyler Witt <tyler@fortyau.com>
2018-02-19 14:53:28 -0600

    Loggin redux

--- lib/classnavapi_web/helpers/chat_helper.ex
+++ lib/classnavapi_web/helpers/chat_helper.ex
@@ -2,8 +2,8 @@ defmodule ClassnavapiWeb.Helpers.ChatHelper do
   def is_liked([]), do: false
   def is_liked(enum, student_id) do
     require Logger
-    Logger.info(inspect(enum))
-    Logger.info(student_id)
-    enum |> Enum.any?(& &1.student_id == student_id)
+    t = enum |> Enum.any?(& &1.student_id == student_id)
+    Logger.info(t)
+    t
   end
 end
