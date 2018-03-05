443eec18b8f9ea6a3a9ee94744cdc40c59850acd
Tyler Witt <tyler@fortyau.com>
2018-03-05 15:18:03 -0600

    Added a delete clause for student chat

--- lib/classnavapi_web/controllers/v1/admin/class/chat_reply_controller.ex
+++ lib/classnavapi_web/controllers/v1/admin/class/chat_reply_controller.ex
@@ -14,6 +14,19 @@ defmodule ClassnavapiWeb.Api.V1.Admin.Class.ChatReplyController do
   plug :check_chat_enabled
   plug :verify_member, :class
 
+  def delete(%{assigns: %{user: %{student: %{id: student_id}}}} = conn, %{"id" => id}) do
+    reply = Repo.get_by!(Reply, student_id: student_id, id: id)
+    case Repo.delete(reply) do
+      {:ok, _struct} ->
+        conn
+        |> send_resp(200, "")
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(ClassnavapiWeb.ChangesetView, "error.json", changeset: changeset)
+    end
+  end
+
   def delete(conn, %{"id" => id}) do
     reply = Repo.get!(Reply, id)
     case Repo.delete(reply) do
