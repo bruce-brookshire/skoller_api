3b57ab382e4ced395c44279f775ec215687335e9
Tyler Witt <tyler@fortyau.com>
2018-03-06 16:59:12 -0600

    Cascade delete docs when class is deleted

---
+++ priv/repo/migrations/20180306225530_cascade_delete_class_docs.exs
@@ -0,0 +1,10 @@
+defmodule Classnavapi.Repo.Migrations.CascadeDeleteClassDocs do
+  use Ecto.Migration
+
+  def change do
+    drop constraint("docs", "docs_class_id_fkey")
+    alter table(:docs) do
+      modify :class_id, references(:classes, on_delete: :delete_all)
+    end
+  end
+end
