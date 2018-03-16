d5ecbfbb418a0ff9fc4dc869a228365825e4adb3
Tyler Witt <tyler@fortyau.com>
2018-03-16 14:42:54 -0500

    Changed migration to have an up and down.

--- priv/repo/migrations/20180315190916_convert_grade_scale_to_blob.exs
+++ priv/repo/migrations/20180315190916_convert_grade_scale_to_blob.exs
@@ -3,7 +3,7 @@ defmodule Classnavapi.Repo.Migrations.ConvertGradeScaleToBlob do
 
   alias Classnavapi.Repo
 
-  def change do
+  def up do
     alter table(:classes) do
       add :grade_scale_map, :map
     end
@@ -12,6 +12,12 @@ defmodule Classnavapi.Repo.Migrations.ConvertGradeScaleToBlob do
     |> Repo.all()
     |> Enum.map(&Ecto.Changeset.change(&1, %{grade_scale_map: convert_gs_to_map(&1.grade_scale)}))
     |> Enum.each(&Repo.update!(&1))
+  end
+
+  def down do
+    alter table(:classes) do
+      remove :grade_scale_map
+    end
   end
 
   defp convert_gs_to_map(gs) do
