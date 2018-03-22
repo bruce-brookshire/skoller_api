61ef68fe480cc885671a5fc7779a3dd8ae7ff416
Tyler Witt <tyler@fortyau.com>
2018-03-22 09:52:03 -0500

    Added auto un-reading of assignments.

--- lib/classnavapi/class/student_assignment.ex
+++ lib/classnavapi/class/student_assignment.ex
@@ -23,6 +23,7 @@ defmodule Classnavapi.Class.StudentAssignment do
       field :is_completed, :boolean
       field :is_notifications, :boolean, default: true
       field :notes, :string
+      field :is_read, :boolean, default: true
       belongs_to :student_class, StudentClass, define_field: false
       belongs_to :weight, Weight, define_field: false
       belongs_to :assignment, Assignment, define_field: false
@@ -35,7 +36,7 @@ defmodule Classnavapi.Class.StudentAssignment do
     @opt_fields [:assignment_id, :weight_id, :is_completed]
     @all_fields @req_fields ++ @opt_fields
 
-    @upd_req_fields [:name, :is_notifications, :due]
+    @upd_req_fields [:name, :is_notifications, :due, :is_read]
     @upd_opt_fields [:weight_id, :is_completed, :notes]
     @upd_fields @upd_opt_fields ++ @upd_req_fields
 
