a0f98193ab091de748145c6fe0bf552b03bcb05e
Tyler Witt <tyler@fortyau.com>
2018-03-27 16:22:41 -0500

    Split assignment reminder query to be one chunk for today reminders, and another for future reminders.

--- lib/classnavapi_web/jobs/send_notifications.ex
+++ lib/classnavapi_web/jobs/send_notifications.ex
@@ -17,6 +17,10 @@ defmodule ClassnavapiWeb.Jobs.SendNotifications do
     time 
     |> assignment_query() 
     |> Enum.each(&send_notifications(&1))
+
+    time
+    |> future_assignment_query()
+    |> Enum.each(&send_future_notifications(&1))
   end
 
   defp assignment_query(time) do
@@ -32,7 +36,26 @@ defmodule ClassnavapiWeb.Jobs.SendNotifications do
     |> where([student], student.notification_time == ^time)
     |> where([student], student.is_notifications == true and student.is_reminder_notifications == true)
     |> where([student, sclass, sassign], not(is_nil(sassign.due)))
-    |> where([student, sclass, sassign], fragment("?::date", sassign.due) >= ^now and fragment("?::date", sassign.due) <= date_add(^now, student.notification_days_notice, "day"))
+    |> where([student, sclass, sassign], fragment("?::date", sassign.due) == ^now)
+    |> where([student, sclass, sassign, class, user, device], sclass.is_dropped == false)
+    |> select([student, sclass, sassign, class, user, device], %{udid: device.udid, device_type: device.type, class_name: class.name, assign_name: sassign.name, assign_due: sassign.due})
+    |> Repo.all()
+  end
+
+  defp future_assignment_query(time) do
+    {:ok, time} = Time.new(time.hour, time.minute, 0, 0)
+    now = DateTime.utc_now() |> DateTime.to_date()
+
+    from(student in Student)
+    |> join(:inner, [student], sclass in StudentClass, student.id == sclass.student_id and sclass.is_notifications == true)
+    |> join(:inner, [student, sclass], sassign in StudentAssignment, sassign.student_class_id == sclass.id and sassign.is_notifications == true)
+    |> join(:inner, [student, sclass, sassign], class in Class, class.id == sclass.class_id)
+    |> join(:inner, [student, sclass, sassign, class], user in User, user.student_id == student.id)
+    |> join(:inner, [student, sclass, sassign, class, user], device in Device, user.id == device.user_id)
+    |> where([student], student.notification_time == ^time)
+    |> where([student], student.is_notifications == true and student.is_reminder_notifications == true)
+    |> where([student, sclass, sassign], not(is_nil(sassign.due)))
+    |> where([student, sclass, sassign], fragment("?::date", sassign.due) > ^now and fragment("?::date", sassign.due) < date_add(^now, student.notification_days_notice, "day"))
     |> where([student, sclass, sassign, class, user, device], sclass.is_dropped == false)
     |> select([student, sclass, sassign, class, user, device], %{udid: device.udid, device_type: device.type, class_name: class.name, assign_name: sassign.name, assign_due: sassign.due})
     |> Repo.all()
@@ -42,7 +65,15 @@ defmodule ClassnavapiWeb.Jobs.SendNotifications do
     Notification.create_notification(assignment.udid, format_msg(assignment), @assignment_reminder_category)
   end
 
+  defp send_future_notifications(assignment) do
+    Notification.create_notification(assignment.udid, format_future_msg(assignment), @assignment_reminder_category)
+  end
+
   defp format_msg(assignment) do
+    assignment.assign_name <> " in " <> assignment.class_name <> " is due " <> due_days_away(assignment.assign_due) 
+  end
+
+  defp format_future_msg(assignment) do
     assignment.assign_name <> " in " <> assignment.class_name <> " is due " <> due_days_away(assignment.assign_due) 
   end
 
