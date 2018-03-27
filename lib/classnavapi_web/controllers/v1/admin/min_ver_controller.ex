542faf1659002070594baea16296feb803ac3921
Tyler Witt <tyler@fortyau.com>
2018-03-27 12:57:42 -0500

    Changed update min ver setting to take an array of settings.

--- lib/classnavapi_web/controllers/v1/admin/min_ver_controller.ex
+++ lib/classnavapi_web/controllers/v1/admin/min_ver_controller.ex
@@ -3,6 +3,8 @@ defmodule ClassnavapiWeb.Api.V1.Admin.MinVerController do
 
   alias Classnavapi.Admin.Settings
   alias ClassnavapiWeb.Admin.SettingView
+  alias ClassnavapiWeb.Helpers.RepoHelper
+  alias Classnavapi.Repo
 
   import ClassnavapiWeb.Helpers.AuthPlug
 
@@ -10,15 +12,27 @@ defmodule ClassnavapiWeb.Api.V1.Admin.MinVerController do
 
   plug :verify_role, %{role: @admin_role}
 
-  def update(conn, %{"name" => name} = params) do
-    old_setting = Settings.get_setting_by_name!(name)
-    case Settings.update_setting(old_setting, params) do
-      {:ok, setting} ->
-        render(conn, SettingView, "show.json", setting: setting)
-      {:error, changeset} ->
+  def update(conn, %{"settings" => settings}) do
+    multi = Ecto.Multi.new()
+    |> Ecto.Multi.run(:settings, &update_settings(settings, &1))
+   
+    case Repo.transaction(multi) do
+      {:ok, _map} ->
+        settings = Settings.get_min_ver_settings()
+        render(conn, SettingView, "index.json", settings: settings)
+      {:error, _, failed_value, _} ->
         conn
-        |> put_status(:unprocessable_entity)
-        |> render(ClassnavapiWeb.ChangesetView, "error.json", changeset: changeset)
+        |> RepoHelper.multi_error(failed_value)
     end
+  end
+
+  defp update_settings(settings, _) do
+    status = settings |> Enum.map(&update_setting(&1))
+    status |> Enum.find({:ok, status}, &RepoHelper.errors(&1))
+  end
+
+  defp update_setting(%{"name" => name} = params) do
+    settings_old = Settings.get_setting_by_name!(name)
+    Settings.update_setting(settings_old, params)
   end
 end
