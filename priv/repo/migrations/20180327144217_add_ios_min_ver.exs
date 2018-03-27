2c47550528dbd988311c8dd2432366294b744a40
Tyler Witt <tyler@fortyau.com>
2018-03-27 10:50:59 -0500

    Added Android version.

--- priv/repo/migrations/20180327144217_add_ios_min_ver.exs
+++ priv/repo/migrations/20180327144217_add_ios_min_ver.exs
@@ -5,10 +5,12 @@ defmodule Classnavapi.Repo.Migrations.AddIosMinVer do
   alias Classnavapi.Admin.Setting
 
   def up do
-    Repo.insert!(%Setting{name: "min_ios_version", value: "999.999.999", topic: "MinVersions"})
+    Repo.insert!(%Setting{name: "min_ios_version", value: "0.0.0", topic: "MinVersions"})
+    Repo.insert!(%Setting{name: "min_android_version", value: "0.0.0", topic: "MinVersions"})
   end
 
   def down do
-    Repo.delete!(%Setting{name: "min_ios_version", value: "999.999.999", topic: "MinVersions"})
+    Repo.delete!(%Setting{name: "min_ios_version", value: "0.0.0", topic: "MinVersions"})
+    Repo.delete!(%Setting{name: "min_android_version", value: "0.0.0", topic: "MinVersions"})
   end
 end
