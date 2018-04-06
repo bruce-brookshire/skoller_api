6d0ee8d8bf1dd23697f330254ab24d0fdc6234da
Tyler Witt <tyler@fortyau.com>
2018-04-06 10:39:33 -0500

    Added school fields and updated address storage.

--- lib/classnavapi/schools/school.ex
+++ lib/classnavapi/schools/school.ex
@@ -13,12 +13,14 @@ defmodule Classnavapi.Schools.School do
   alias Classnavapi.Schools.ClassPeriod
 
   schema "schools" do
-    field :adr_city, :string
+    field :adr_country, :string
+    field :adr_locality, :string
     field :adr_line_1, :string
     field :adr_line_2, :string
-    field :adr_state, :string
+    field :adr_line_3, :string
+    field :adr_region, :string
     field :adr_zip, :string
-    field :is_readonly, :boolean
+    field :is_readonly, :boolean, default: false
     field :is_diy_enabled, :boolean
     field :is_diy_preferred, :boolean
     field :is_auto_syllabus, :boolean
@@ -27,14 +29,15 @@ defmodule Classnavapi.Schools.School do
     field :short_name, :string
     field :is_chat_enabled, :boolean, default: true
     field :is_assignment_posts_enabled, :boolean, default: true
+    field :is_university, :boolean, default: true
     has_many :class_periods, ClassPeriod
     has_many :classes, through: [:class_periods, :classes]
 
     timestamps()
   end
 
-  @req_fields [:name, :adr_line_1, :adr_city, :adr_state, :adr_zip, :timezone, :is_chat_enabled, :is_assignment_posts_enabled]
-  @opt_fields [:adr_line_2, :is_readonly,
+  @req_fields [:name, :adr_locality, :adr_region, :timezone, :is_chat_enabled, :is_assignment_posts_enabled, :is_university, :adr_country]
+  @opt_fields [:adr_line_1, :adr_line_2, :adr_zip, :is_readonly, :adr_line_3,
               :is_diy_enabled, :is_diy_preferred, :is_auto_syllabus, :short_name]
   @all_fields @req_fields ++ @opt_fields
   @upd_fields @all_fields
