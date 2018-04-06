7ee7cfe916276471e43162698211357192fd8795
Tyler Witt <tyler@fortyau.com>
2018-04-06 14:51:20 -0500

    Funky fresh migration for splitting class number.

---
+++ priv/repo/migrations/20180406184209_split_class_values.exs
@@ -0,0 +1,22 @@
+defmodule Skoller.Repo.Migrations.SplitClassValues do
+  use Ecto.Migration
+
+  def change do
+    drop index("classes", [], name: :unique_class_index)
+    rename table("classes"), :number, to: :code
+    alter table(:classes) do
+      add :section, :string
+      add :subject, :string
+    end
+    create unique_index(:classes, [:class_period_id, 
+                                    :professor_id,
+                                    :campus,
+                                    :name,
+                                    :code,
+                                    :section,
+                                    :subject,
+                                    :meet_days,
+                                    :meet_end_time,
+                                    :meet_start_time], name: :unique_class_index)
+  end
+end
