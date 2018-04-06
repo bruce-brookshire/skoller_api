72cf04fc6b283355e9d7064a46fd7289379bcf2f
Tyler Witt <tyler@fortyau.com>
2018-04-06 14:46:16 -0500

    Updated migration

--- priv/repo/migrations/20171214170709_add_unique_index_to_class.exs
+++ priv/repo/migrations/20171214170709_add_unique_index_to_class.exs
@@ -8,8 +8,6 @@ defmodule Skoller.Repo.Migrations.AddUniqueIndexToClass do
                                     :name,
                                     :number,
                                     :meet_days,
-                                    :class_start,
-                                    :class_end,
                                     :meet_end_time,
                                     :meet_start_time], name: :unique_class_index)
   end
