7f28b1fc011b7faae9388bf9a01c4bc6c9a4e00f
Tyler Witt <tyler@fortyau.com>
2018-04-09 14:27:06 -0500

    Rolled up more queries into contexts.

--- lib/skoller/students/students.ex
+++ lib/skoller/students/students.ex
@@ -11,6 +11,9 @@ defmodule Skoller.Students do
   alias Skoller.School.FieldOfStudy
   alias Skoller.School.StudentField
   alias Skoller.Classes
+  alias Skoller.Class.Status
+  alias Skoller.Professor
+  alias Skoller.Schools.ClassPeriod
 
   import Ecto.Query
 
@@ -144,9 +147,167 @@ defmodule Skoller.Students do
     |> Repo.aggregate(:count, :id)
   end
 
+  @doc """
+   Shows all `Skoller.Schools.Class`. Can be used as a search with multiple filters.
+
+  ## Behavior:
+   Only searches the current `Skoller.Schools.ClassPeriod`
+
+  ## Filters:
+  * school
+    * `Skoller.Schools.School` :id
+  * professor.name
+    * `Skoller.Professor` :name
+  * class.status
+    * `Skoller.Class.Status` :id
+    * For ghost classes, use 0.
+  * class.name
+    * `Skoller.Schools.Class` :name
+  * class.number
+    * `Skoller.Schools.Class` :number
+  * class.meet_days
+    * `Skoller.Schools.Class` :meet_days
+  * class.length
+    * 1st Half
+    * 2nd Half
+    * Full Term
+    * Custom
+
+  ## Returns:
+  * 422 `SkollerWeb.ChangesetView`
+  * 401
+  * 200 `SkollerWeb.Class.SearchView`
+  """
+  def get_classes_with_enrollment(params) do
+    #TODO: Filter ClassPeriod
+    from(class in Class)
+    |> join(:inner, [class], period in ClassPeriod, class.class_period_id == period.id)
+    |> join(:left, [class], prof in Professor, class.professor_id == prof.id)
+    |> join(:inner, [class, period, prof], school in School, school.id == period.school_id)
+    |> join(:inner, [class, period, prof, school], status in Status, status.id == class.class_status_id)
+    |> join(:left, [class, period, prof, school, status], enroll in subquery(count_subquery()), enroll.class_id == class.id)
+    |> where([class, period, prof], ^filter(params))
+    |> select([class, period, prof, school, status, enroll], %{class: class, class_period: period, professor: prof, school: school, class_status: status, enroll: enroll})
+    |> Repo.all()
+  end
+
+  defp filter(%{} = params) do
+    dynamic = params["or"] != "true"
+
+    dynamic
+    |> school_filter(params)
+    |> prof_filter(params)
+    |> prof_id_filter(params)
+    |> status_filter(params)
+    |> ghost_filter(params)
+    |> maint_filter(params)
+    |> name_filter(params)
+    |> number_filter(params)
+    |> day_filter(params)
+  end
+
+  defp school_filter(dynamic, %{"school" => filter, "or" => "true"}) do
+    dynamic([class, period, prof], period.school_id == ^filter or ^dynamic)
+  end
+  defp school_filter(dynamic, %{"school" => filter}) do
+    dynamic([class, period, prof], period.school_id == ^filter and ^dynamic)
+  end
+  defp school_filter(dynamic, _), do: dynamic
+
+  defp prof_filter(dynamic, %{"professor_name" => filter, "or" => "true"}) do
+    prof_filter = filter <> "%"
+    dynamic([class, period, prof], ilike(prof.name_last, ^prof_filter) or ilike(prof.name_first, ^prof_filter) or ^dynamic)
+  end
+  defp prof_filter(dynamic, %{"professor_name" => filter}) do
+    prof_filter = filter <> "%"
+    dynamic([class, period, prof], (ilike(prof.name_last, ^prof_filter) or ilike(prof.name_first, ^prof_filter)) and ^dynamic)
+  end
+  defp prof_filter(dynamic, _), do: dynamic
+
+  defp prof_id_filter(dynamic, %{"professor_id" => filter, "or" => "true"}) do
+    dynamic([class, period, prof], prof.id == ^filter or ^dynamic)
+  end
+  defp prof_id_filter(dynamic, %{"professor_id" => filter}) do
+    dynamic([class, period, prof], prof.id == ^filter and ^dynamic)
+  end
+  defp prof_id_filter(dynamic, _), do: dynamic
+
+  defp status_filter(dynamic, %{"class_status" => "0", "or" => "true"}) do
+    dynamic([class, period, prof], class.is_ghost == true or ^dynamic)
+  end
+  defp status_filter(dynamic, %{"class_status" => "100", "or" => "true"}) do
+    dynamic([class, period, prof], class.is_new_class == true or ^dynamic)
+  end
+  defp status_filter(dynamic, %{"class_status" => filter, "or" => "true"}) do
+    dynamic([class, period, prof], class.class_status_id == ^filter or ^dynamic)
+  end
+  defp status_filter(dynamic, %{"class_status" => "0"}) do
+    dynamic([class, period, prof], class.is_ghost == true and ^dynamic)
+  end
+  defp status_filter(dynamic, %{"class_status" => "100"}) do
+    dynamic([class, period, prof], class.is_new_class == true and ^dynamic)
+  end
+  defp status_filter(dynamic, %{"class_status" => filter}) do
+    dynamic([class, period, prof], class.class_status_id == ^filter and ^dynamic)
+  end
+  defp status_filter(dynamic, _), do: dynamic
+
+  defp ghost_filter(dynamic, %{"class_status" => "0"}), do: dynamic
+  defp ghost_filter(dynamic, %{"ghost" => "true"}) do
+    dynamic([class, period, prof], class.is_ghost == true and ^dynamic)
+  end
+  defp ghost_filter(dynamic, %{"ghost" => "false"}) do
+    dynamic([class, period, prof], class.is_ghost == false and ^dynamic)
+  end
+  defp ghost_filter(dynamic, _), do: dynamic
+
+  defp maint_filter(dynamic, %{"class_maint" => "true"}) do
+    dynamic([class, period, prof], class.is_editable == false and ^dynamic)
+  end
+  defp maint_filter(dynamic, %{"class_maint" => "false"}) do
+    dynamic([class, period, prof], class.is_editable == true and ^dynamic)
+  end
+  defp maint_filter(dynamic, _), do: dynamic
+
+  defp name_filter(dynamic, %{"class_name" => filter, "or" => "true"}) do
+    name_filter = "%" <> filter <> "%"
+    dynamic([class, period, prof], ilike(class.name, ^name_filter) or ^dynamic)
+  end
+  defp name_filter(dynamic, %{"class_name" => filter}) do
+    name_filter = "%" <> filter <> "%"
+    dynamic([class, period, prof], ilike(class.name, ^name_filter) and ^dynamic)
+  end
+  defp name_filter(dynamic, _), do: dynamic
+
+  defp number_filter(dynamic, %{"class_number" => filter, "or" => "true"}) do
+    number_filter = "%" <> filter <> "%"
+    dynamic([class, period, prof], ilike(class.number, ^number_filter) or ^dynamic)
+  end
+  defp number_filter(dynamic, %{"class_number" => filter}) do
+    number_filter = "%" <> filter <> "%"
+    dynamic([class, period, prof], ilike(class.number, ^number_filter) and ^dynamic)
+  end
+  defp number_filter(dynamic, _), do: dynamic
+
+  defp day_filter(dynamic, %{"class_meet_days" => filter, "or" => "true"}) do
+    dynamic([class, period, prof], class.meet_days == ^filter or ^dynamic)
+  end
+  defp day_filter(dynamic, %{"class_meet_days" => filter}) do
+    dynamic([class, period, prof], class.meet_days == ^filter and ^dynamic)
+  end
+  defp day_filter(dynamic, _), do: dynamic
+
   defp get_school_enrollment_subquery() do
     from(s in subquery(get_schools_for_student_subquery()))
     |> group_by([s], s.school_id)
     |> select([s], %{school_id: s.school_id, count: count(s.student_id)})
+  end
+
+  defp count_subquery() do
+    from(c in Class)
+    |> join(:left, [c], sc in StudentClass, c.id == sc.class_id)
+    |> where([c, sc], sc.is_dropped == false)
+    |> group_by([c, sc], c.id)
+    |> select([c, sc], %{class_id: c.id, count: count(sc.id)})
   end
 end
