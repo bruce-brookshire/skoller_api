e55c9518fc6801f7e711c6689aacfbbc35ad68fa
Tyler Witt <tyler@fortyau.com>
2018-04-10 11:24:30 -0500

    rolled up some mo.

--- lib/skoller_web/controllers/v1/student/class/grade_controller.ex
+++ lib/skoller_web/controllers/v1/student/class/grade_controller.ex
@@ -4,11 +4,9 @@ defmodule SkollerWeb.Api.V1.Student.Class.GradeController do
   alias Skoller.Class.StudentAssignment
   alias Skoller.Repo
   alias SkollerWeb.Class.StudentAssignmentView
-  alias Skoller.Class.StudentClass
-  alias Skoller.Schools.Class
+  alias Skoller.Students
 
   import SkollerWeb.Helpers.AuthPlug
-  import Ecto.Query
   
   @student_role 100
   
@@ -16,7 +14,7 @@ defmodule SkollerWeb.Api.V1.Student.Class.GradeController do
   plug :verify_member, %{of: :student_assignment, using: :assignment_id}
 
   def create(conn, %{"assignment_id" => assignment_id} = params) do
-    case get_student_assignment(assignment_id) do
+    case Students.get_student_assignment_by_id(assignment_id) do
       nil ->
         conn
         |> send_resp(401, "")
@@ -33,14 +31,5 @@ defmodule SkollerWeb.Api.V1.Student.Class.GradeController do
             |> render(SkollerWeb.ChangesetView, "error.json", changeset: changeset)
         end
     end
-  end
-
-  defp get_student_assignment(id) do
-    from(sa in StudentAssignment)
-    |> join(:inner, [sa], sc in StudentClass, sc.id == sa.student_class_id)
-    |> join(:inner, [sa, sc], class in Class, sc.class_id == class.id)
-    |> where([sa], sa.id == ^id)
-    |> where([sa, sc, class], class.is_editable == true)
-    |> Repo.one()
   end
 end
