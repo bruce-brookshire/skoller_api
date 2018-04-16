9c5ad07640b4a5adccdfa53e586ad1bd34aaea1f
Tyler Witt <tyler@fortyau.com>
2018-04-16 14:51:39 -0500

    Added university insert changeset.

--- lib/skoller/users/user.ex
+++ lib/skoller/users/user.ex
@@ -48,6 +48,19 @@ defmodule Skoller.Users.User do
     |> put_pass_hash()
   end
 
+  @doc false
+  def changeset_insert_university(%User{} = user, attrs) do
+    user
+    |> cast(attrs, @all_fields)
+    |> validate_required(@req_fields)
+    |> update_change(:email, &String.downcase(&1))
+    |> unique_constraint(:email)
+    |> cast_assoc(:student)
+    |> validate_format(:email, ~r/@.+\.edu$/)
+    |> put_pass_hash()
+  end
+
+  @doc false
   def changeset_update(%User{} = user, attrs) do
     user
     |> cast(attrs, @upd_fields)
@@ -56,6 +69,7 @@ defmodule Skoller.Users.User do
     |> put_pass_hash()
   end
 
+  @doc false
   def changeset_update_admin(%User{} = user, attrs) do
     user
     |> cast(attrs, @adm_upd_fields)
