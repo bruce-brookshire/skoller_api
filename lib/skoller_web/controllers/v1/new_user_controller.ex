15517089514f1edde77caaa753af64bccda43f3e
Tyler Witt <tyler@fortyau.com>
2018-04-17 09:12:58 -0500

    Fixed some more bugs.

--- lib/skoller_web/controllers/v1/new_user_controller.ex
+++ lib/skoller_web/controllers/v1/new_user_controller.ex
@@ -12,17 +12,22 @@ defmodule SkollerWeb.Api.V1.NewUserController do
     multi = Ecto.Multi.new
     |> Ecto.Multi.run(:user, &create_user(params, &1))
     |> Ecto.Multi.run(:token, &TokenHelper.login(&1.user))
+    |> Repo.transaction()
 
-    case Repo.transaction(multi) do
-      {:ok, %{user: %{user: %{student: student} = user}, token: token}} ->
-        student.phone |> Sms.verify_phone(student.verification_code)
+    case multi do
+      {:ok, %{user: %{user: user}, token: token}} ->
+        user = user |> Repo.preload(:student)
+        user.student |> send_verification_text()
         render(conn, AuthView, "show.json", [user: user, token: token])
-      {:ok, %{} = auth} ->
-        render(conn, AuthView, "show.json", auth: auth)
       {:error, _, failed_value, _} ->
         conn
         |> RepoHelper.multi_error(failed_value)
     end
+  end
+
+  defp send_verification_text(nil), do: nil
+  defp send_verification_text(student) do
+    student.phone |> Sms.verify_phone(student.verification_code)
   end
 
   defp create_user(params, _) do
