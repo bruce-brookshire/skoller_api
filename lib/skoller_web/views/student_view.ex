791a820b0bf25b69ab92cc03050dfc86ce1be02f
Tyler Witt <tyler@fortyau.com>
2018-05-07 14:57:22 -0500

    Change users to user.

--- lib/skoller_web/views/student_view.ex
+++ lib/skoller_web/views/student_view.ex
@@ -49,7 +49,7 @@ defmodule SkollerWeb.StudentView do
       name_last: student.name_last,
       organization: student.organization,
       bio: student.bio,
-      users: render_one(student.users |> List.first(), UserView, "user.json")
+      user: render_one(student.users |> List.first(), UserView, "user.json")
     }
   end
 end
