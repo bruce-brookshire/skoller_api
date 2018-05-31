a88b3e79c51fb0b4b52be6867c1c6cf0d99e505b
Tyler Witt <tyler@fortyau.com>
2018-05-31 10:21:20 -0500

    Added custom link enrollment abilities.

--- lib/skoller/custom_signups/custom_signups.ex
+++ lib/skoller/custom_signups/custom_signups.ex
@@ -1,5 +1,6 @@
 defmodule Skoller.CustomSignups do
   alias Skoller.CustomSignups.Link
+  alias Skoller.CustomSignups.Signup
   alias Skoller.Repo
 
   def create_link(params) do
@@ -16,9 +17,17 @@ defmodule Skoller.CustomSignups do
     Repo.get(Link, id)
   end
 
+  def get_link_by_link(link) do
+    Repo.get_by(Link, link: link)
+  end
+
   def update_link(link_old, params) do
     link_old
     |> Link.changeset_update(params)
     |> Repo.update()
+  end
+
+  def track_signup(student_id, link_id) do
+    Repo.insert(%Signup{custom_signup_link_id: link_id, student_id: student_id})
   end
 end
