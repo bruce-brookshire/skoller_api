4b94a323a22bcd213ffd8ff854c16a00b065a24b
Tyler Witt <tyler@fortyau.com>
2018-06-01 11:18:31 -0500

    Added reported by column and is_completed bool.

--- lib/skoller/users/report.ex
+++ lib/skoller/users/report.ex
@@ -3,23 +3,35 @@ defmodule Skoller.Users.Report do
   import Ecto.Changeset
   alias Skoller.Users.Report
 
-
   schema "user_reports" do
     field :context, :string
     field :note, :string
     field :user_id, :id
+    field :is_complete, :boolean, default: false
+    field :reported_by, :id
 
     timestamps()
   end
 
-  @req_fields [:user_id, :context]
+  @req_fields [:user_id, :context, :reported_by]
   @opt_fields [:note]
   @all_fields @req_fields ++ @opt_fields
+
+  @upd_req [:is_complete]
+  @upd_all @upd_req
 
   @doc false
   def changeset(%Report{} = report, attrs) do
     report
     |> cast(attrs, @all_fields)
     |> validate_required(@req_fields)
+    |> foreign_key_constraint(:user_id)
+    |> foreign_key_constraint(:reported_by)
+  end
+
+  def changeset_update(%Report{} = report, attrs) do
+    report
+    |> cast(attrs, @upd_all)
+    |> validate_required(@upd_req)
   end
 end
