dc649c260506a6f8e8bb6c0eeaef985046256f4e
Tyler Witt <tyler@fortyau.com>
2018-06-08 16:39:37 -0500

    Sammi data handling updated.

--- lib/skoller/sammi.ex
+++ lib/skoller/sammi.ex
@@ -7,10 +7,10 @@ defmodule Skoller.Sammi do
   require Logger
 
   def sammi(_params, nil), do: nil
-  def sammi(%{"is_syllabus" => "true", "class_id" => class_id} = params, file) do
+  def sammi(%{"is_syllabus" => "true", "class_id" => class_id}, file) do
     case Sammi.Api.extract(file) do
       {:ok, sammi} ->
-        Logger.info(sammi)
+        Logger.info(inspect(sammi))
         sammi |> add_grade_scale(class_id)
         sammi |> add_professor_info(class_id)
       {:error, val} ->
@@ -21,11 +21,9 @@ defmodule Skoller.Sammi do
   end
   def sammi(_params, _file), do: nil
 
-  defp add_grade_scale(%{"grade_scale" => %{"value" => ""}}, _class_id), do: nil
-  defp add_grade_scale(%{"grade_scale" => %{"value" => val}}, class_id) do
-    val = val
-    |> String.trim()
-    |> String.trim("|")
+  defp add_grade_scale(%{"grade_scale" => %{"values" => %{}}}, _class_id), do: nil
+  defp add_grade_scale(%{"grade_scale" => %{"values" => val}}, class_id) do
+    val = val |> String.trim()
     class_id
     |> Classes.get_class_by_id!()
     |> Classes.update_class(%{"grade_scale" => val})
@@ -51,22 +49,22 @@ defmodule Skoller.Sammi do
     |> get_email(professor_info, professor)
   end
 
-  defp get_office_hours(map, %{"office_hours" => %{"value" => ""}}, %{office_availability: nil}), do: map |> Map.put("office_availability", nil)
-  defp get_office_hours(map, %{"office_hours" => %{"value" => val}}, %{office_availability: nil}) do 
+  defp get_office_hours(map, %{"office_hours" => ""}, %{office_availability: nil}), do: map |> Map.put("office_availability", nil)
+  defp get_office_hours(map, %{"office_hours" => val}, %{office_availability: nil}) do 
     val = val |> String.trim()
     map |> Map.put("office_availability", val)
   end
   defp get_office_hours(map, _params, %{office_availability: _val}), do: map
 
-  defp get_office_location(map, %{"office_location" => %{"value" => ""}}, %{office_location: nil}), do: map |> Map.put("office_location", nil)
-  defp get_office_location(map, %{"office_location" => %{"value" => val}}, %{office_location: nil}) do 
+  defp get_office_location(map, %{"office_location" => ""}, %{office_location: nil}), do: map |> Map.put("office_location", nil)
+  defp get_office_location(map, %{"office_location" => val}, %{office_location: nil}) do 
     val = val |> String.trim()
     map |> Map.put("office_location", val)
   end
   defp get_office_location(map, _params, %{office_location: _val}), do: map
 
-  defp get_phone(map, %{"phone" => %{"value" => ""}}, %{phone: nil}), do: map |> Map.put("phone", nil)
-  defp get_phone(map, %{"phone" => %{"value" => val}}, %{phone: nil}) do
+  defp get_phone(map, %{"phone" => ""}, %{phone: nil}), do: map |> Map.put("phone", nil)
+  defp get_phone(map, %{"phone" => val}, %{phone: nil}) do
     val = val |> String.trim()
     case val |> String.match?(~r/^([0-9]{3}-)?[0-9]{3}-[0-9]{4}$/) do
       true -> map |> Map.put("phone", val)
@@ -75,8 +73,8 @@ defmodule Skoller.Sammi do
   end
   defp get_phone(map, _params, %{phone: _val}), do: map
 
-  defp get_email(map, %{"email" => %{"value" => ""}}, %{email: nil}), do: map |> Map.put("email", nil)
-  defp get_email(map, %{"email" => %{"value" => val}}, %{email: nil}) do
+  defp get_email(map, %{"email" => ""}, %{email: nil}), do: map |> Map.put("email", nil)
+  defp get_email(map, %{"email" => val}, %{email: nil}) do
     val = val |> String.trim()
     case val |> String.match?(~r/.{2,}@/) do
       true -> map |> Map.put("email", val)
