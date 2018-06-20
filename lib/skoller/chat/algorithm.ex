d973c4aba04e1a8fc7fa3157aeb0177e84a2dd22
Tyler Witt <tyler@fortyau.com>
2018-06-20 11:30:21 -0500

    More commenting.

--- lib/skoller/chat/algorithm.ex
+++ lib/skoller/chat/algorithm.ex
@@ -2,7 +2,9 @@ defmodule Skoller.Chat.Algorithm do
   @moduledoc false
   
   use Ecto.Schema
-
+  
+  # The primary key is a normal, non-incrementing ID. Seeded by seed
+  # file or migration.
   @primary_key {:id, :id, []}
   schema "chat_algorithms" do
     field :name, :string
