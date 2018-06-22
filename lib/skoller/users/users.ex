10c609e8252827286d115271d65432516f4d9505
Tyler Witt <tyler@fortyau.com>
2018-06-22 10:15:44 -0500

    Finished users context.

--- lib/skoller/users/users.ex
+++ lib/skoller/users/users.ex
@@ -18,22 +18,54 @@ defmodule Skoller.Users do
 
   @student_role 100
 
+  @doc """
+  Reports a user
+
+  ## Returns
+  `{:ok, Skoller.Users.Report}` or `{:error, Ecto.Changeset}`
+  """
   def report_user(params) do
     %Report{}
     |> Report.changeset(params)
     |> Repo.insert()
   end
 
+  @doc """
+  Gets a user by id with `:roles` and `:student`
+
+  ## Returns
+  `Skoller.Users.User` or `Ecto.NoResultsError`
+  """
   def get_user_by_id!(id) do
     Repo.get!(User, id)
     |> Repo.preload([:roles, :student])
   end
 
+  @doc """
+  Gets a user by id with `:roles` and `:student`
+
+  ## Returns
+  `Skoller.Users.User` or `nil`
+  """
   def get_user_by_id(id) do
     Repo.get(User, id)
     |> Repo.preload([:roles, :student])
   end
 
+  @doc """
+  Creates a user, with student if included in `params`.
+
+  ## Params
+   * `%{"student" => %{"link" => enrollment_link}}`, will add enrolled_by to student based on the link.
+   * `%{"student" => %{"custom_link" => custom_link}}`, will add student to custom link enrollment.
+
+  ## Opts
+   * `[admin: true]`, will verify without text.
+
+  # Returns
+  `{:ok, %{user: Skoller.Users.User, roles: [Skoller.UserRole], field_of_study: Skoller.Students.FieldOfStudy, custom_link: Skoller.CustomSignups.Signup || {:ok, nil}, link: String}}`
+  or `{:error, failed_val}`
+  """
   def create_user(params, opts \\ []) do
     multi = %User{}
     |> User.changeset_insert(params)
@@ -52,6 +84,13 @@ defmodule Skoller.Users do
     end
   end
 
+  @doc """
+  Updates a user, with student if included in params.
+
+  # Returns
+  `{:ok, %{user: Skoller.Users.User, roles: [Skoller.UserRole], field_of_study: Skoller.Students.FieldOfStudy, link: String}}`
+  or `{:error, _, failed_val, _}`
+  """
   def update_user(user_old, params) do
     changeset = User.changeset_update_admin(user_old, params)
     Ecto.Multi.new
@@ -62,23 +101,47 @@ defmodule Skoller.Users do
     |> Repo.transaction()
   end
 
+  @doc """
+  Gets a user by email.
+
+  ## Returns
+  `Skoller.Users.User` or `nil`
+  """
   def get_user_by_email(email) do
     Repo.get_by(User, email: String.downcase(email))
   end
 
+  @doc """
+  Changes a user's password
+
+  ## Returns
+  `{:ok, Skoller.Users.User}` or `{:error, Ecto.Changeset}`
+  """
   def change_password(user_old, password) do
     user_old
     |> User.changeset_update(%{"password" => password})
     |> Repo.update()
   end
 
-  def get_users_in_class(id) do
+  @doc """
+  Gets the users enrolled in a class.
+  
+  ## Returns
+  `[Skoller.Users.User]` or `[]`
+  """
+  def get_users_in_class(class_id) do
     from(u in User)
     |> join(:inner, [u], sc in subquery(Students.get_enrolled_student_classes_subquery()), sc.student_id == u.student_id)
-    |> where([u, sc], sc.class_id == ^id)
+    |> where([u, sc], sc.class_id == ^class_id)
     |> Repo.all()
   end
 
+  @doc """
+  Gets the locks by class.
+
+  ## Returns
+  `[%{lock: Skoller.Locks.Lock, user: Skoller.Users.User}]` or `[]`
+  """
   def get_user_locks_by_class(class_id) do
     from(l in Lock)
     |> join(:inner, [l], u in User, l.user_id == u.id)
@@ -87,6 +150,7 @@ defmodule Skoller.Users do
     |> Repo.all()
   end
 
+  # Generates a verification code if admin: true is not passed in through opts.
   defp verification_code(%Ecto.Changeset{valid?: true, changes: %{student: %Ecto.Changeset{valid?: true} = s_changeset}} = u_changeset, opts) do
     case get_opt(opts, :admin) do
       "true" -> 
@@ -97,6 +161,7 @@ defmodule Skoller.Users do
   end
   defp verification_code(changeset, _opts), do: changeset
 
+  # Verifies a student if admin: true passed in though opts.
   defp verify_student(%Ecto.Changeset{valid?: true, changes: %{student: %Ecto.Changeset{valid?: true} = s_changeset}} = u_changeset, opts) do
     case get_opt(opts, :admin) do
       "true" -> 
@@ -107,6 +172,7 @@ defmodule Skoller.Users do
   end
   defp verify_student(changeset, _opts), do: changeset
 
+  # Adds enrolled_by if "link" is present in params.
   defp get_enrolled_by(%Ecto.Changeset{valid?: true, changes: %{student: %Ecto.Changeset{valid?: true} = s_changeset}} = u_changeset, %{"student" => %{"link" => link}}) do
     enrolled_by = Repo.get_by(Student, enrollment_link: link)
     case enrolled_by do
@@ -116,6 +182,7 @@ defmodule Skoller.Users do
   end
   defp get_enrolled_by(changeset, _params), do: changeset
 
+  # The standard insert user multi.
   defp insert_user(changeset, params) do
     Ecto.Multi.new
     |> Ecto.Multi.insert(:user, changeset)
@@ -123,11 +190,13 @@ defmodule Skoller.Users do
     |> Ecto.Multi.run(:fields_of_study, &add_fields_of_study(&1, params))
   end
 
+  # Gets a link for a student.
   defp get_link(%User{student: %Student{} = student}) do
     Students.generate_student_link(student)
   end
   defp get_link(_user), do: {:ok, nil}
 
+  # Adds student to custom link enrollment if the custom link exists.
   defp custom_link_signup(%User{student: %Student{} = student}, %{"student" => %{"custom_link" => link}}) do
     case CustomSignups.get_link_by_link(link) do
       nil -> {:ok, nil}
@@ -186,6 +255,7 @@ defmodule Skoller.Users do
     Students.delete_fields_of_study_by_student_id(id)
   end
 
+  # Parses a keylist and gets the value of the atom.
   defp get_opt(opts, atom) do
     case opts |> List.keytake(atom, 0) do
       nil -> nil
