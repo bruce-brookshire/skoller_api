fa03b1e24d95a7b083386c76b62496af0139f875
Tyler Witt <tyler@fortyau.com>
2018-06-22 11:47:38 -0500

    Documented the chat channel even though no one uses it...poor duder.

--- lib/skoller_web/channels/chat_channel.ex
+++ lib/skoller_web/channels/chat_channel.ex
@@ -1,4 +1,42 @@
 defmodule SkollerWeb.ChatChannel do
+  @moduledoc """
+  A Channel for using chat and posting messages through a websocket.
+  
+  ## Join a class chat
+  To join a class chat, send a topic of "chat:`class_id`"
+
+  #### Returns
+   * If joined successfully, the socket is maintained.
+   * If the class has chat disabled, `{reason: "Chat disabled"}` is returned.
+   * If the student is not authorized, `{error: "unauthorized"}` is returned.
+  
+  ## To post
+  To post in a class, send a topic of post, with `{body: String}`
+  The post will broadcast.
+
+  #### Returns
+   * If successful, the socket is maintained.
+   * Otherwise, `{error: "Failed to create post"}` is returned.
+  
+  ## To comment
+  To comment in a class, send a topic of comment, with `{body: String, post_id: Id}`
+  The comment will broadcast.
+
+  #### Returns
+   * If successful, the socket is maintained.
+   * Otherwise, `{error: "Failed to add comment"}` is returned.
+
+  ## To reply
+  To reply in a class, send a topic of reply, with `{body: String, comment_id: Id}`
+  The reply will broadcast.
+
+  #### Returns
+   * If successful, the socket is maintained.
+   * Otherwise, `{error: "Failed to reply"}` is returned.
+  """
+
+  # This socket is not currently used anywhere.
+  # Testing this is really difficult. Try to find a websocket client.
   use Phoenix.Channel
 
   alias Skoller.Repo
@@ -9,6 +47,8 @@ defmodule SkollerWeb.ChatChannel do
   alias Skoller.Schools
   alias Skoller.Students
 
+  @doc false
+  # Joins a class after checking that the class has chat enabled and the student is enrolled.
   def join("chat:" <> class_id, _params, socket) do
     case get_class_enabled(class_id) do
       {:ok, _val} ->
@@ -21,6 +61,9 @@ defmodule SkollerWeb.ChatChannel do
     end
   end
 
+  @doc false
+  # Handles incoming requests.
+  # Broadcast will send an update to all users.
   def handle_in("post", %{"body" => body}, socket) do
     "chat:" <> class_id = socket.topic
     changeset = Post.changeset(%Post{}, %{class_id: class_id, student_id: socket.assigns.user.student.id, post: body})
@@ -32,7 +75,6 @@ defmodule SkollerWeb.ChatChannel do
         {:reply, %{error: "Failed to create post"}}
     end
   end
-
   def handle_in("comment", %{"body" => body, "post_id" => post_id}, socket) do
     changeset = Comment.changeset(%Comment{}, %{chat_post_id: post_id, student_id: socket.assigns.user.student.id, comment: body})
     case Repo.insert(changeset) do
@@ -43,7 +85,6 @@ defmodule SkollerWeb.ChatChannel do
         {:reply, %{error: "Failed to add comment"}}
     end
   end
-
   def handle_in("reply", %{"body" => body, "comment_id" => comment_id}, socket) do
     changeset = Reply.changeset(%Reply{}, %{chat_comment_id: comment_id, student_id: socket.assigns.user.student.id, reply: body})
     case Repo.insert(changeset) do
