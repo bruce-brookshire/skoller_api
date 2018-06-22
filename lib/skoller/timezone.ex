48d601f0a2c42f01c0009a6cb7bb31dd2be0a3e9
Tyler Witt <tyler@fortyau.com>
2018-06-22 10:32:49 -0500

    Timezone documentation.

--- lib/skoller/timezone.ex
+++ lib/skoller/timezone.ex
@@ -1,8 +1,21 @@
 defmodule Skoller.Timezone do
+  @moduledoc """
+  Timezone API module.
+  """
   require Logger
 
   @get_tz_url "get-time-zone"
 
+  @doc """
+  Gets the timezone for a given `locality`, `country`, and `region`.
+
+  Locality is the City, Region is the State. Region is not required if the country is not the US.
+
+  ## Returns
+   * `{:ok, String}` if one timezone is found. The timezone name is the long form name (i.e. "America/Chicago")
+   * `{:ok, nil}` if no timezone is found, or multiple are found.
+   * `{:error, error}` if there is an error.
+  """
   def get_timezone(locality, country, region \\ nil) do
     case request(@get_tz_url, %{by: "city", country: country, region: region, city: locality}) do
       {:ok, %{"zones" => zones}} ->
@@ -18,6 +31,7 @@ defmodule Skoller.Timezone do
     end
   end
 
+  # This sends the request and logs errors/returns responses.
   defp request(url, params) do
     case Timezone.get(url, [], params: params) do
       {:ok, response} ->
