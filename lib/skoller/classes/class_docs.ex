81273d47c146bf3361bdb0aaf4f9e34721ff0478
Tyler Witt <tyler@fortyau.com>
2018-06-29 13:37:03 -0500

    Moved more stuff into class docs. Removed other Sammi data handling.

--- lib/skoller/classes/class_docs.ex
+++ lib/skoller/classes/class_docs.ex
@@ -7,6 +7,8 @@ defmodule Skoller.ClassDocs do
   alias Ecto.UUID
   alias Skoller.Repo
   alias Skoller.Class.Doc
+  alias Skoller.MapErrors
+  alias Skoller.Classes
 
   require Logger
 
@@ -49,5 +51,49 @@ defmodule Skoller.ClassDocs do
   """
   def delete(%Doc{} = doc) do
     Repo.delete(doc)
+  end
+
+  @doc """
+  Inserts a doc
+
+  ## Returns
+  `{:ok, %Skoller.Class.Doc{}}` or `{:error, %Ecto.Changeset{}}`
+  """
+  def insert(params) do
+    Doc.changeset(%Doc{}, params)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Inserts a doc into multiple classes.
+
+  May cause class status updates.
+
+  ## Returns
+  `{:ok, Map}` or `{:error, _, _, _}` where `Map` contains
+   * `{:doc, [Skoller.Class.Doc]}`
+   * `{:status, [Skoller.Classes.Status]}`
+  """
+  def multi_insert_docs(classes, params) do
+    Ecto.Multi.new
+    |> Ecto.Multi.run(:doc, &insert_class_doc(classes, params, &1))
+    |> Ecto.Multi.run(:status, &check_statuses(classes, &1.doc))
+    |> Repo.transaction()
+  end
+
+  defp check_statuses(classes, docs) do
+    status = classes |> Enum.map(&Classes.check_status(&1, %{doc: elem(docs |> Enum.find(fn(x) -> elem(x, 1).class_id == &1.id end), 1)}))
+    status |> Enum.find({:ok, status}, &MapErrors.check_tuple(&1))
+  end
+
+  defp insert_class_doc(classes, params, _) do
+    status = classes |> Enum.map(&insert_doc(&1, params))
+    status |> Enum.find({:ok, status}, &MapErrors.check_tuple(&1))
+  end
+
+  defp insert_doc(class, params) do
+    params 
+    |> Map.put("class_id", class.id)
+    |> insert()
   end
 end
