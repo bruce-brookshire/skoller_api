3073f42c6d3b404723ab778752cc79f8c1075dc4
Tyler Witt <tyler@fortyau.com>
2018-06-29 12:26:14 -0500

    Refactor chat posts

--- lib/skoller_web/controllers/v1/admin/assignment/post_controller.ex
+++ lib/skoller_web/controllers/v1/admin/assignment/post_controller.ex
@@ -3,8 +3,8 @@ defmodule SkollerWeb.Api.V1.Admin.Assignment.PostController do
 
   use SkollerWeb, :controller
   
-  alias Skoller.Repo
-  alias Skoller.Assignment.Post
+  alias Skoller.ChatPosts
+  alias SkollerWeb.ChangesetView
 
   import SkollerWeb.Plugs.Auth
   import SkollerWeb.Plugs.ChatAuth
@@ -16,29 +16,24 @@ defmodule SkollerWeb.Api.V1.Admin.Assignment.PostController do
   plug :check_chat_enabled, :assignment
   plug :verify_member, :class_assignment
 
-  def delete(%{assigns: %{user: %{student: %{id: student_id}}}} = conn, %{"id" => id}) do
-    post = Repo.get_by!(Post, student_id: student_id, id: id)
-    case Repo.delete(post) do
+  def delete(conn, %{"id" => id}) do
+    post = conn |> get_post(id)
+    case ChatPosts.delete(post) do
       {:ok, _struct} ->
         conn
         |> send_resp(200, "")
       {:error, changeset} ->
         conn
         |> put_status(:unprocessable_entity)
-        |> render(SkollerWeb.ChangesetView, "error.json", changeset: changeset)
+        |> render(ChangesetView, "error.json", changeset: changeset)
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    post = Repo.get!(Post, id)
-    case Repo.delete(post) do
-      {:ok, _struct} ->
-        conn
-        |> send_resp(200, "")
-      {:error, changeset} ->
-        conn
-        |> put_status(:unprocessable_entity)
-        |> render(SkollerWeb.ChangesetView, "error.json", changeset: changeset)
-    end
+  # If a student is attempting to delete, verify it is their post.
+  defp get_post(%{assigns: %{user: %{student: %{id: student_id}}}}, id) do
+    ChatPosts.get_post_by_student_and_id!(student_id, id)
+  end
+  defp get_post(_conn, id) do
+    ChatPosts.get!(id)
   end
 end
