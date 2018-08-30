dba5bb3457b88d9b56fbe7c4e7655e6a7efbcc89
Tyler Witt <tyler@fortyau.com>
2018-08-30 14:16:05 -0500

    Email and notification bases set up.

--- lib/skoller/student_classes/emails.ex
+++ lib/skoller/student_classes/emails.ex
@@ -3,7 +3,27 @@ defmodule Skoller.StudentClasses.Emails do
   Defines emails based on student classes
   """
 
+  alias Skoller.Mailer
+
+  import Bamboo.Email
+
+  require EEx
+  EEx.function_from_file :defp, :build_no_classes_body, System.cwd() <> "/lib/skoller/student_classes/no_classes.html.eex", []
+
+  @from_email "noreply@skoller.co"
+
   def send_no_classes_emails(students) do
-    
+    students
+    |> Enum.map(&build_no_classes_email(List.first(&1.users)))
+    |> Enum.each(&Mailer.deliver_later(&1))
+  end
+
+  defp build_no_classes_email(user) do
+    new_email()
+    |> to(user.email)
+    |> from(@from_email)
+    |> subject("🚨 URGENT: You have no classes 🚨")
+    |> html_body(build_no_classes_body())
+    |> text_body("test")
   end
 end
--- lib/skoller/student_classes/notifications.ex
+++ lib/skoller/student_classes/notifications.ex
@@ -10,10 +10,10 @@ defmodule Skoller.StudentClasses.Notifications do
 
   def send_no_classes_notification(students) do
     students_with_devices = students |> Enum.map(&Map.put(&1, :devices, Devices.get_devices_by_user_id(List.first(&1.users).id)))
-    students_with_devices |> Enum.each(&Enum.each(&1.devices, fn(x) -> Notification.create_notification(x.udid, create_no_class_msg(&1), @no_class_category) end))
+    students_with_devices |> Enum.each(&Enum.each(&1.devices, fn(x) -> Notification.create_notification(x.udid, create_no_class_msg(), @no_class_category) end))
   end
 
-  defp create_no_class_msg(_student_with_devices) do
-    "test"
+  defp create_no_class_msg() do
+    "Looks like you’re not in any classes…Add them today so you don’t get behind! 👍"
   end
 end
