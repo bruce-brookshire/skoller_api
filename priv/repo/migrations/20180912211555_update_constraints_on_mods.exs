3548580020e44a3c60b3f1db67ce195a2e4691e1
Tyler Witt <tyler@fortyau.com>
2018-09-12 16:17:14 -0500

    Updated assignment modifications constraints.

---
+++ priv/repo/migrations/20180912211555_update_constraints_on_mods.exs
@@ -0,0 +1,10 @@
+defmodule Skoller.Repo.Migrations.UpdateConstraintsOnMods do
+  use Ecto.Migration
+
+  def change do
+    drop constraint("assignment_modifications", "assignment_modifications_student_id_fkey")
+    alter table(:assignment_modifications) do
+      modify :student_id, references(:students, on_delete: :nilify_all)
+    end
+  end
+end
