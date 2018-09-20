f76945440368496fcb729f17f6158611f4167fbb
Tyler Witt <tyler@fortyau.com>
2018-09-20 15:12:28 -0500

    Added a function to get the points for a student.

--- lib/skoller/student_points/student_points.ex
+++ lib/skoller/student_points/student_points.ex
@@ -2,4 +2,15 @@ defmodule Skoller.StudentPoints do
   @moduledoc """
   The context module for student points
   """
+
+  alias Skoller.StudentPoints.StudentPoint
+  alias Skoller.Repo
+
+  import Ecto.Query
+
+  def get_points_by_student_id(student_id) do
+    from(sp in StudentPoint)
+    |> where([sp], sp.student_id == ^student_id)
+    |> Repo.aggregate(:sum, :value)
+  end
 end
