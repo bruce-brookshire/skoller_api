9af6012325aa0bb600b61c5b873a69777b03b7b3
Tyler Witt <tyler@fortyau.com>
2018-09-28 10:35:51 -0500

    Fixed bug and added some spacing.

--- lib/skoller/users/notifications.ex
+++ lib/skoller/users/notifications.ex
@@ -7,12 +7,12 @@ defmodule Skoller.Users.Notifications do
   alias Skoller.Devices
   alias Services.Notification
 
-  @link_used_msg "🤩 Someone just signed up for Skoller using your link!"
+  @link_used_msg "🤩  Someone just signed up for Skoller using your link!"
   @link_used_category "SignupLink.Used"
 
   def send_link_used_notification(student_id) do
     user = Students.get_user_by_student_id(student_id)
-    device = Devices.get_devices_by_user_id(user.id)
-    Notification.create_notification(device.udid, device.type, @link_used_msg, @link_used_category)
+    devices = Devices.get_devices_by_user_id(user.id)
+    devices |> Enum.each(&Notification.create_notification(&1.udid, &1.type, @link_used_msg, @link_used_category))
   end
 end
