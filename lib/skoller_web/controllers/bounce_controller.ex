1a3f03ab83a31c119d01c8f0d6210ada2a3beea0
Tyler Witt <tyler@fortyau.com>
2018-10-03 09:03:39 -0500

    Fixed unsubscribe error.

--- lib/skoller_web/controllers/bounce_controller.ex
+++ lib/skoller_web/controllers/bounce_controller.ex
@@ -10,13 +10,7 @@ defmodule SkollerWeb.Api.BounceController do
 
   def bounce(conn, %{"Message" => message}) do
     decoded_message = Poison.decode!(message)
-    case handle_notification(decoded_message) do
-      {:ok, _result} ->
-        conn |> send_resp(204, "")
-      {:error, _reason} ->
-        Logger.error(message)
-        conn |> send_resp(403, "")
-    end
+    handle_notification(decoded_message)
   end
 
   def bounce(conn, params) do
