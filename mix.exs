06c4d6d39319cc59406b3130d98d4e992e719466
Tyler Witt <tyler@fortyau.com>
2018-10-03 12:15:16 -0500

    Updated ver

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Skoller.Mixfile do
   def project do
     [
       app: :skoller,
-      version: "2.2.2",
+      version: "2.3.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
