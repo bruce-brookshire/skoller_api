36c0b003f62d4bbea988b001100015191e390c85
Tyler Witt <tyler@fortyau.com>
2018-10-12 10:43:06 -0500

    Deregister other users from the same device.

--- lib/skoller_web/controllers/v1/device_controller.ex
+++ lib/skoller_web/controllers/v1/device_controller.ex
@@ -11,13 +11,23 @@ defmodule SkollerWeb.Api.V1.DeviceController do
   plug :verify_user
 
   def register(conn, params) do
+    case Devices.get_device_by_attributes(params["udid"], params["type"], params["user_id"]) do
+      nil ->
+        conn |> create_device(params)
+      device ->
+        conn |> render(DeviceView, "device.json", device: device)
+    end
+  end
+
+  defp create_device(conn, params) do
     case Devices.create_device(params) do
       {:ok, user_device} ->
+        Task.start(Devices, :deregister_device_for_other_users_by_udid_and_type, [user_device])
         conn |> render(DeviceView, "device.json", device: user_device)
       {:error, changeset} ->
         conn
         |> put_status(:unprocessable_entity)
         |> render(SkollerWeb.ChangesetView, "error.json", changeset: changeset)
     end
-  end
+  end 
 end
