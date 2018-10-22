3f6575dabe53a0f31de44c0ce7c158f4b3c88078
Tyler Witt <tyler@fortyau.com>
2018-10-22 10:15:27 -0500

    Made marketing email templates more specific so that other templates can be made.

--- lib/skoller/services/mailer/marketing_email.ex
+++ lib/skoller/services/mailer/marketing_email.ex
@@ -2,7 +2,7 @@ defmodule Skoller.Services.MarketingEmail do
   @moduledoc """
   A helper module for sending marketing emails
   """
-  use Bamboo.Phoenix, view: SkollerWeb.EmailView
+  use Bamboo.Phoenix, view: SkollerWeb.MarketingView
 
   alias Skoller.Services.Mailer
   alias Skoller.EmailLogs.EmailLog
@@ -36,7 +36,7 @@ defmodule Skoller.Services.MarketingEmail do
   defp base_email(user_id) do
     new_email()
     |> from(@from_email)
-    |> put_layout({SkollerWeb.LayoutView, :email})
+    |> put_layout({SkollerWeb.LayoutView, :marketing})
     |> assign(:unsub_path, System.get_env("WEB_URL") <> "/unsubscribe/" <> user_id)
   end
 end
