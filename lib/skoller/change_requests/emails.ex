58c2f91453c3feb8fa43defbb0781e40306cf476
Tyler Witt <tyler@fortyau.com>
2018-10-22 12:49:00 -0500

    Converted more emails

--- lib/skoller/change_requests/emails.ex
+++ lib/skoller/change_requests/emails.ex
@@ -4,24 +4,12 @@ defmodule Skoller.ChangeRequests.Emails do
   """
 
   alias Skoller.Repo
-  alias Skoller.Services.Email
-  alias Skoller.Services.Mailer
+  alias Skoller.Services.TriggeredEmail
 
-  import Bamboo.Email
-
-  @from_email "noreply@skoller.co"
   @change_approved " info change has been approved!"
-  @we_approved_change "We have approved your request to change class information for "
-  @ending "We hope you and your classmates have a great semester!"
 
   def send_request_completed_email(user, class) do
     user = user |> Repo.preload(:student)
-    new_email()
-    |> to(user.email)
-    |> from(@from_email)
-    |> subject(class.name <> @change_approved)
-    |> html_body("<p>" <> user.student.name_first <> ",<br /><br >" <> @we_approved_change <> class.name <> "<br /><br />" <> @ending <> "</p>" <> Email.signature())
-    |> text_body(user.student.name_first <> ",\n \n" <> @we_approved_change <> class.name <> "\n \n" <> @ending <> "\n \n" <> Email.text_signature())
-    |> Mailer.deliver_later
+    TriggeredEmail.send_email(user.email, class.name <> @change_approved, :request_completed, [student_name_first: user.student.name_first, class_name: class.name])
   end
 end
