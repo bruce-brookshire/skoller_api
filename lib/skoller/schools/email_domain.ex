1842a116973518dd61945e7978c06248eb082f78
Tyler Witt <tyler@fortyau.com>
2018-11-08 09:26:57 -0600

    Only keep the last section of the email domain

--- lib/skoller/schools/email_domain.ex
+++ lib/skoller/schools/email_domain.ex
@@ -18,5 +18,19 @@ defmodule Skoller.Schools.EmailDomain do
     email_domain
     |> cast(attrs, @all_fields)
     |> validate_required(@req_fields)
+    |> update_change(:email_domain, &get_last_section_of_email_domain(&1))
+  end
+
+  defp get_last_section_of_email_domain(domain) do
+    domain
+    |> String.split(".", trim: true)
+    |> get_last_two_elements()
+    |> Enum.intersperse(".")
+    |> List.to_string()
+  end
+
+  defp get_last_two_elements(list) do
+    len = length(list)
+    Enum.slice((len - 2)..(len - 1))
   end
 end
