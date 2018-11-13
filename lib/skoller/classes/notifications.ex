a07f4a433fae68f328c3e190b6565066e319b2d5
Tyler Witt <tyler@fortyau.com>
2018-11-13 16:56:44 -0600

    Updated class complete notification

--- lib/skoller/classes/notifications.ex
+++ lib/skoller/classes/notifications.ex
@@ -3,7 +3,6 @@ defmodule Skoller.Classes.Notifications do
   A context module for class notifications.
   """
 
-  alias Skoller.Repo
   alias Skoller.Notifications
   alias Skoller.Services.Notification
   alias Skoller.Devices
@@ -12,12 +11,8 @@ defmodule Skoller.Classes.Notifications do
   @class_complete_category "Class.Complete"
   @manual_syllabus_category "Manual.NeedsSyllabus"
 
-  @class_complete "We didn't find any assignments on the syllabus. Be sure to add assignments on the app throughout the semester so you and your classmates can keep up. 💯"  
-  @is_ready " is ready!"
-  @we_created "We created"
-  @from_syllabus "from the class syllabus. Any new assignments or schedule changes are up to you and your classmates. 💯"
-  @one_assign_class_complete "assignment " <> @from_syllabus
-  @multiple_assign_class_complete "assignments " <> @from_syllabus
+  @a_classmate "A classmate has started setting up "
+  @check_out " . Check out the assignments start rolling in your schedule 💯  👍  "
 
   @needs_syllabus_msg "It’s not too late to upload your syllabi on our website! Take a couple minutes to knock it out. Your class will love you for it 👌"
 
@@ -25,10 +20,9 @@ defmodule Skoller.Classes.Notifications do
     devices = class.id
             |> Notifications.get_users_from_class()
             |> Enum.reduce([], &Devices.get_devices_by_user_id(&1.id) ++ &2)
-    class = class |> Repo.preload([:assignments])
-    msg = class.assignments |> class_complete_msg()
+    msg =  class |> class_complete_msg()
     
-    devices |> Enum.each(&Notification.create_notification(&1.udid, &1.type, %{title: class.name <> @is_ready, body: msg}, @class_complete_category))
+    devices |> Enum.each(&Notification.create_notification(&1.udid, &1.type, msg, @class_complete_category))
   end
   def send_class_complete_notification(_class), do: :ok
 
@@ -42,11 +36,7 @@ defmodule Skoller.Classes.Notifications do
     |> Enum.each(&Notification.create_notification(&1.udid, &1.type, @needs_syllabus_msg, @manual_syllabus_category))
   end
 
-  defp class_complete_msg(assignments) do
-    case Enum.count(assignments) do
-      0 -> @class_complete
-      1 -> @we_created <> " 1 " <> @one_assign_class_complete
-      num -> @we_created <> " " <> to_string(num) <> " " <> @multiple_assign_class_complete
-    end
+  defp class_complete_msg(class) do
+    @a_classmate <> class.name <> @check_out
   end
 end
