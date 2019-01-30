243b83278c8d8f613b8f07680f32f0aa7465fc49
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2019-01-30 16:10:00 -0600

    script for creating fake users for staging using carson's junk email

--- lib/skoller/student_classes/jobs.ex
+++ lib/skoller/student_classes/jobs.ex
@@ -34,6 +34,7 @@ defmodule Skoller.StudentClasses.Jobs do
         if email_type.is_active_notification do
           students |> Notifications.send_no_classes_notification(email_type)
         end
+
         if email_type.is_active_email do
           students |> Emails.send_no_classes_emails()
         end
@@ -51,6 +52,7 @@ defmodule Skoller.StudentClasses.Jobs do
         if email_type.is_active_notification do
           students |> Notifications.send_needs_setup_notification(email_type)
         end
+        
         if email_type.is_active_email do
           students |> Emails.send_needs_setup_emails()
         end
