d06c4a97d4602e420f155301cb71899bd61d9866
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2019-02-18 18:35:52 -0600

    oops

--- lib/skoller/custom_signups/custom_signups.ex
+++ lib/skoller/custom_signups/custom_signups.ex
@@ -90,7 +90,7 @@ defmodule Skoller.CustomSignups do
     from(s in Signup)
       |> join(:inner, [s], l in Link, s.custom_signup_link_id == l.id)
       |> where([s, l], s.student_id == ^student_id)
-      |> select([s, l], s.name)
+      |> select([s, l], l.name)
       |> Repo.one()
   end
 
