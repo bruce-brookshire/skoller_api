2c2b3e1a7ab1af0e5fc0148503a909ebc537a3c8
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2019-08-07 11:28:07 -0500

    changes for jon

--- lib/skoller_web/views/student/class/link_view.ex
+++ lib/skoller_web/views/student/class/link_view.ex
@@ -3,18 +3,21 @@ defmodule SkollerWeb.Student.Class.LinkView do
   use SkollerWeb, :view
 
   alias SkollerWeb.Student.Class.LinkView
+  alias SkollerWeb.ClassView
 
   def render("show.json", %{link: link}) do
     render_one(link, LinkView, "link.json")
   end
 
-  def render("link.json", %{link: link}) do
+  def render("link.json", %{link: %{class: student_class} = link}) do
     student = link.student |> Skoller.Repo.preload([:users])
+    IO.inspect student_class
+
     %{
-      class_name: link.class.name,
       student_name_first: student.name_first,
       student_name_last: student.name_last,
-      student_image_path: (student.users |> List.first).pic_path
+      student_image_path: (student.users |> List.first()).pic_path,
+      student_class: render_one(student_class, ClassView, "show.json")
     }
   end
 end
