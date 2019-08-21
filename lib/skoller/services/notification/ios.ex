01b6d0e581a83167e4c54d973c8fab971988c7cd
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2019-08-21 16:20:02 -0500

    notifications are working properly now

--- lib/skoller/services/notification/ios.ex
+++ lib/skoller/services/notification/ios.ex
@@ -26,9 +26,7 @@ defmodule Skoller.Services.Notification.Ios do
     |> put_custom(custom)
     |> put_category(category)
     |> put_sound("default")
-    |> APNS.push()
-    |> IO.inspect()
-    # |> APNS.push(on_response: &handle_result/1)
+    |> APNS.push(on_response: &handle_result/1)
   end
 
   def create_notification(device, msg, category, custom) do
@@ -37,8 +35,7 @@ defmodule Skoller.Services.Notification.Ios do
     |> put_category(category)
     |> put_sound("default")
     |> put_custom(custom)
-    |> APNS.push()
-    |> IO.inspect()
+    |> APNS.push(on_response: &handle_result/1)
   end
 
   def handle_result(response) do
