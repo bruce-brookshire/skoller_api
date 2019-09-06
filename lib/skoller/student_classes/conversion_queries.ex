7a0c9395dcc60a7fd3ccbadb2bfc00a61d0c069a
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2019-09-06 13:37:16 -0500

    reworked the join second class domain to require a class size of at least 4

--- lib/skoller/student_classes/conversion_queries.ex
+++ lib/skoller/student_classes/conversion_queries.ex
@@ -17,9 +17,14 @@ defmodule Skoller.StudentClasses.ConversionQueries do
   import Ecto.Query
 
   @days_of_week ["M", "T", "W", "R", "F", "S", "U"]
+
+  @party_size 4
+
   @needs_setup_status_id 1100
   @class_setup_status_id 1400
   @class_issue_status_id 1500
+
+
 
   @doc """
   Gets a list of users that are not enrolled in a class.
@@ -85,7 +90,7 @@ defmodule Skoller.StudentClasses.ConversionQueries do
     |> where(
       [c, cp, s, sc, u, sl, o, a],
       (c.class_status_id == @class_setup_status_id or c.class_status_id == @class_issue_status_id) and
-        sc.is_dropped == false and a.active < 4
+        sc.is_dropped == false and a.active < @party_size
     )
     |> where(fragment("meet_days LIKE ?", ^day_of_week))
     |> where(fragment("meet_start_time <= (CURRENT_TIME AT TIME ZONE timezone)::time"))
@@ -119,8 +124,14 @@ defmodule Skoller.StudentClasses.ConversionQueries do
       a_n in subquery(student_enrollment_not_setup_subquery()),
       on: a_n.student_id == u.student_id
     )
-    |> where(
+    |> join(
+      :left,
       [c, cp, s, sc, u, sl, o, a_s, a_n],
+      class_stat in subquery(party_sized_classes_subquery()),
+      on: c.id == class_stat.class_id
+    )
+    |> where(
+      [c, cp, s, sc, u, sl, o, a_s, a_n, c_stat],
       a_s.enrollment_count == 1 and (is_nil(a_n.enrollment_count) or a_n.enrollment_count == 0) and
         sc.is_dropped == false
     )
@@ -130,28 +141,13 @@ defmodule Skoller.StudentClasses.ConversionQueries do
         "meet_start_time > ((CURRENT_TIME - INTERVAL '1 minutes' * 5) AT TIME ZONE timezone)::time"
       )
     )
-    |> select([c, cp, s, sc, u, sl, o, a_s, a_n], %{
+    |> where([c, cp, s, sc, u, sl, o, a_s, a_n, c_stat], c_stat.count >= @party_size)
+    |> select([c, cp, s, sc, u, sl, o, a_s, a_n, c_stat], %{
       user: u,
       class_name: c.name,
       org: o
     })
     |> Repo.all()
-  end
-
-  def student_enrollment_setup_subquery() do
-    from(sc in StudentClass)
-    |> join(:inner, [sc], c in Class, on: c.id == sc.class_id)
-    |> where([sc, c], sc.is_dropped == false and c.class_status_id == @class_setup_status_id)
-    |> group_by([sc, c], sc.student_id)
-    |> select([sc, c], %{student_id: sc.student_id, enrollment_count: count(sc.student_id)})
-  end
-
-  def student_enrollment_not_setup_subquery() do
-    from(sc in StudentClass)
-    |> join(:inner, [sc], c in Class, on: c.id == sc.class_id)
-    |> where([sc, c], sc.is_dropped == false and c.class_status_id != @class_setup_status_id)
-    |> group_by([sc, c], sc.student_id)
-    |> select([sc, c], %{student_id: sc.student_id, enrollment_count: count(sc.student_id)})
   end
 
   defp base_student_class_query() do
@@ -164,5 +160,27 @@ defmodule Skoller.StudentClasses.ConversionQueries do
     |> join(:left, [c, cp, s, sc, u, sl], o in Organization,
       on: sl.custom_signup_link_id == o.custom_signup_link_id
     )
+  end
+
+  defp student_enrollment_setup_subquery() do
+    from(sc in StudentClass)
+    |> join(:inner, [sc], c in Class, on: c.id == sc.class_id)
+    |> where([sc, c], sc.is_dropped == false and c.class_status_id == @class_setup_status_id)
+    |> group_by([sc, c], sc.student_id)
+    |> select([sc, c], %{student_id: sc.student_id, enrollment_count: count(sc.student_id)})
+  end
+
+  defp student_enrollment_not_setup_subquery() do
+    from(sc in StudentClass)
+    |> join(:inner, [sc], c in Class, on: c.id == sc.class_id)
+    |> where([sc, c], sc.is_dropped == false and c.class_status_id != @class_setup_status_id)
+    |> group_by([sc, c], sc.student_id)
+    |> select([sc, c], %{student_id: sc.student_id, enrollment_count: count(sc.student_id)})
+  end
+
+  defp party_sized_classes_subquery() do
+    from(sc in StudentClass)
+    |> group_by([sc], sc.class_id)
+    |> select([sc], %{class_id: sc.class_id, count: count(sc.class_id)})
   end
 end
