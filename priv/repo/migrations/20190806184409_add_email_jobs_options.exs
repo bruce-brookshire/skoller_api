f27ea4236a0b76371d49ec9a8ee1e775061973ce
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2019-09-09 14:17:28 -0500

    Email jobs now use maps for options. Notifications updated to reflect new structure as well

--- priv/repo/migrations/20190806184409_add_email_jobs_options.exs
+++ priv/repo/migrations/20190806184409_add_email_jobs_options.exs
@@ -9,25 +9,25 @@ defmodule Skoller.Repo.Migrations.AddEmailJobsOptions do
       add(:options, {:map, :string})
     end
 
-    # Repo.insert!(%EmailType{
-    #   id: 500,
-    #   name: "Grow Community Email",
-    #   category: "Class.Community",
-    #   is_active_email: false,
-    #   is_active_notification: false
-    # })
+    Repo.insert!(%EmailType{
+      id: 500,
+      name: "Grow Community Email",
+      category: "Class.Community",
+      is_active_email: false,
+      is_active_notification: false
+    })
 
-    # Repo.insert!(%EmailType{
-    #   id: 600,
-    #   name: "Join Second Class",
-    #   category: "Class.JoinSecond",
-    #   is_active_email: false,
-    #   is_active_notification: false
-    # })
+    Repo.insert!(%EmailType{
+      id: 600,
+      name: "Join Second Class",
+      category: "Class.JoinSecond",
+      is_active_email: false,
+      is_active_notification: false
+    })
 
-    # EmailType
-    # |> Repo.get_by(id: 400)
-    # |> Repo.delete()
+    EmailType
+    |> Repo.get_by(id: 400)
+    |> Repo.delete()
   end
 
   def down do
@@ -35,19 +35,19 @@ defmodule Skoller.Repo.Migrations.AddEmailJobsOptions do
       remove(:options)
     end
 
-    # EmailType
-    # |> Repo.get_by(id: 500)
-    # |> Repo.delete()
+    EmailType
+    |> Repo.get_by(id: 500)
+    |> Repo.delete()
 
-    # EmailType
-    # |> Repo.get_by(id: 600)
-    # |> Repo.delete()
+    EmailType
+    |> Repo.get_by(id: 600)
+    |> Repo.delete()
 
-    # Repo.insert!(%EmailType{
-    #   id: 400,
-    #   name: "Class Start Airdrop",
-    #   category: "Class.Start",
-    #   is_active_notification: false
-    # })
+    Repo.insert!(%EmailType{
+      id: 400,
+      name: "Class Start Airdrop",
+      category: "Class.Start",
+      is_active_notification: false
+    })
   end
 end
