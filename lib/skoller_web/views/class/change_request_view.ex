dd2a2ffba8a92278c9be27dd6d7234943ad93419
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2019-09-14 13:45:17 -0500

    updated

--- lib/skoller_web/views/class/change_request_view.ex
+++ lib/skoller_web/views/class/change_request_view.ex
@@ -17,7 +17,7 @@ defmodule SkollerWeb.Class.ChangeRequestView do
       id: change_request.id,
       change_type: render_one(change_request.class_change_type, TypeView, "type.json")
       data: change_request.data,
-      inserted_at: change_request.inserted_at
+      inserted_at: change_request.inserted_at,
       is_completed: change_request.is_completed,
       note: change_request.note,
       user: render_one(change_request.user, UserView, "user.json"),
