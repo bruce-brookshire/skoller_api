708ddfccabd1e21934fbba3a40e7e936eacf8199
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2019-10-23 13:45:09 -0500

    422 on completing an already completed member

--- lib/skoller_web/controllers/v1/admin/class/change_request_member_controller.ex
+++ lib/skoller_web/controllers/v1/admin/class/change_request_member_controller.ex
@@ -13,8 +13,6 @@ defmodule SkollerWeb.Api.V1.Admin.Class.ChangeRequestMemberController do
   plug :verify_role, %{roles: [@change_req_role, @admin_role]}
 
   def complete(conn, %{"id" => id} = params) do
-    IO.inspect params
-    IO.inspect conn
     case ChangeRequestMembers.set_completed(id) do
       {:ok, %{change_request_member_update: member}} ->
         render(conn, ChangeRequestView, "show.json", change_request_member: member)
@@ -22,6 +20,9 @@ defmodule SkollerWeb.Api.V1.Admin.Class.ChangeRequestMemberController do
       {:error, _, failed_value, _} ->
         conn
         |> MultiError.render(failed_value)
+
+      nil ->
+        conn |> MultiError.render(%{"error" => "Member is already completed"})
     end
   end
 end
