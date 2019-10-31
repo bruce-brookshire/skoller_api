2dcb46617adbc4af59103cd117f7303a3af85751
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2019-10-31 12:01:17 -0500

    get student_classes by start date, not by is dropped

--- lib/skoller/enrolled_students/enrolled_students.ex
+++ lib/skoller/enrolled_students/enrolled_students.ex
@@ -4,10 +4,11 @@ defmodule Skoller.EnrolledStudents do
   """
 
   alias Skoller.StudentClasses.StudentClass
-  alias Skoller.Repo
-  alias Skoller.Classes.Schools
+  alias Skoller.Periods.ClassPeriod
   alias Skoller.Students.Student
+  alias Skoller.Classes.Schools
   alias Skoller.Classes.Class
+  alias Skoller.Repo
 
   import Ecto.Query
 
@@ -20,7 +21,9 @@ defmodule Skoller.EnrolledStudents do
   """
   def preload_enrolled_classes(student_id) do
     from(c in Class)
-    |> join(:inner, [c], sc in subquery(get_enrolled_classes_by_student_id_subquery(student_id)), on: sc.class_id == c.id)
+    |> join(:inner, [c], sc in subquery(get_enrolled_classes_by_student_id_subquery(student_id)),
+      on: sc.class_id == c.id
+    )
   end
 
   @doc """
@@ -31,7 +34,9 @@ defmodule Skoller.EnrolledStudents do
   """
   def get_students_by_class(class_id) do
     from(s in Student)
-    |> join(:inner, [s], sc in subquery(get_enrollment_by_class_id_subquery(class_id)), on: s.id == sc.student_id)
+    |> join(:inner, [s], sc in subquery(get_enrollment_by_class_id_subquery(class_id)),
+      on: s.id == sc.student_id
+    )
     |> Repo.all()
   end
 
@@ -42,16 +47,30 @@ defmodule Skoller.EnrolledStudents do
   """
   def enrolled_student_class_subquery() do
     from(sc in StudentClass)
-    |> where([sc], sc.is_dropped == false)
+    |> join(:inner, [sc], c in Class, on: c.id == sc.class_id)
+    |> join(:inner, [sc, c], p in ClassPeriod, on: p.id == sc.class_period_id)
+    |> where(
+      [sc, c, p],
+      fragment("(? - current_timestamp) < interval '30 days'", p.start_date) and
+        fragment("(current_timestamp - ?) > interval '15 days'", p.end_date)
+    )
+    |> select([sc, c, p], sc)
   end
 
   @doc """
   Subquery for getting enrolled student classes by student.
   """
   def get_enrolled_classes_by_student_id_subquery(student_id) do
-    #TODO: Filter ClassPeriod
     from(sc in StudentClass)
-    |> where([sc], sc.student_id == ^student_id and sc.is_dropped == false)
+    |> join(:inner, [sc], c in Class, on: c.id == sc.class_id)
+    |> join(:inner, [sc, c], p in ClassPeriod, on: p.id == sc.class_period_id)
+    |> where([sc, c, p], sc.student_id == ^student_id)
+    |> where(
+      [sc, c, p],
+      fragment("(? - current_timestamp) < interval '30 days'", p.start_date) and
+        fragment("(current_timestamp - ?) > interval '15 days'", p.end_date)
+    )
+    |> select([sc, c, p], sc)
   end
 
   @doc """
@@ -64,7 +83,7 @@ defmodule Skoller.EnrolledStudents do
 
   """
   def get_enrolled_classes_by_student_id(student_id) do
-    #TODO: Filter ClassPeriod
+    # TODO: Filter ClassPeriod
     from(classes in subquery(get_enrolled_classes_by_student_id_subquery(student_id)))
     |> Repo.all()
     |> Repo.preload(:class)
@@ -77,8 +96,9 @@ defmodule Skoller.EnrolledStudents do
   `true` if a student can enroll in more classes, otherwise `false`
   """
   def check_enrollment_limit_for_student(student_id) do
-    class_count = from(classes in subquery(get_enrolled_classes_by_student_id_subquery(student_id)))
-    |> Repo.aggregate(:count, :id)
+    class_count =
+      from(classes in subquery(get_enrolled_classes_by_student_id_subquery(student_id)))
+      |> Repo.aggregate(:count, :id)
 
     class_count < @enrollment_limit
   end
@@ -156,7 +176,9 @@ defmodule Skoller.EnrolledStudents do
   """
   def get_enrolled_student_classes_subquery(params \\ %{}) do
     from(sc in StudentClass)
-    |> join(:inner, [sc], c in subquery(Schools.get_school_from_class_subquery(params)), on: c.class_id == sc.class_id)
+    |> join(:inner, [sc], c in subquery(Schools.get_school_from_class_subquery(params)),
+      on: c.class_id == sc.class_id
+    )
     |> where([sc], sc.is_dropped == false)
   end
 
@@ -167,13 +189,17 @@ defmodule Skoller.EnrolledStudents do
    * `%{"school_id" => school_id}`, filters on school.
   """
   def get_student_subquery(_params \\ %{})
+
   def get_student_subquery(%{"school_id" => _school_id} = params) do
     from(s in Student)
     |> join(:inner, [s], sc in StudentClass, on: sc.student_id == s.id)
-    |> join(:inner, [s, sc], c in subquery(Schools.get_school_from_class_subquery(params)), on: c.class_id == sc.class_id)
+    |> join(:inner, [s, sc], c in subquery(Schools.get_school_from_class_subquery(params)),
+      on: c.class_id == sc.class_id
+    )
     |> where([s, sc], sc.is_dropped == false)
     |> distinct([s], s.id)
   end
+
   def get_student_subquery(_params) do
     from(s in Student)
   end
