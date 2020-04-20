249a8c485681610b7d888df872fb05e24c69e6bb
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2020-04-20 17:00:02 -0500

    associations now preload on the return route

--- lib/skoller/adapter.ex
+++ lib/skoller/adapter.ex
@@ -1,24 +1,37 @@
 defmodule Skoller.Adapter do
-  defmacro __using__([model: model]) do
+  defmacro __using__(model: model) do
     quote do
       alias Skoller.Repo
       alias unquote(model), as: Model
-      
+
       import Ecto.Query
 
-      def get_by_id(id), do: Repo.get(Model, id)
+      def get_by_id(id), do: Repo.get(Model, id) |> preload()
 
       def index(query_params) when is_list(query_params),
         do:
           from(m in Model, where: ^query_params)
           |> Repo.all()
+          |> Enum.map(&preload/1)
 
-      def update(id, %{} = params) when is_integer(id), do: get_by_id(id) |> __MODULE__.update(params)
+      def update(id, %{} = params) when is_integer(id),
+        do: get_by_id(id) |> __MODULE__.update(params)
 
       def update(%Model{} = object, %{} = params),
-        do: object |> Model.changeset(params) |> Repo.update()
+        do: object |> Model.changeset(params) |> Repo.update() |> preload()
 
-      def create(%{} = params), do: params |> Model.insert_changeset() |> Repo.insert()
+      def create(%{} = params),
+        do: params |> Model.insert_changeset() |> Repo.insert() |> preload()
+
+      defp preload({:ok, %{} = model}), do: {:ok, preload(model)}
+
+      defp preload(%{} = model) do
+        fields = Model.__schema__(:associations)
+
+        Repo.preload(model, fields)
+      end
+
+      defp preload(error), do: error
 
       defoverridable create: 1, update: 2, index: 1, get_by_id: 1
     end
--- lib/view.ex
+++ lib/view.ex
@@ -10,20 +10,28 @@ defmodule SkollerWeb.View do
       use SkollerWeb, :view
 
       alias unquote(model), as: Model
+      alias Ecto.Association.NotLoaded
 
       def single_atom, do: unquote(single_atom)
       def plural_atom, do: unquote(plural_atom)
 
-      def render("show.json", %{unquote(single_atom) => object}) do
-        fields = Model.__schema__(:fields)
-
-        Map.take(object, fields)
+      def render("show.json", %{unquote(single_atom) => model}) do
+        fields = Model.__schema__(:fields) |> Enum.map(& {&1, Map.get(model, &1)})
+        associations = Model.__schema__(:associations) |> Enum.map(& {&1, render_association(Map.get(model, &1))})
+        
+        fields ++ associations 
+        |> Map.new
       end
 
-      def render("index.json", %{unquote(plural_atom) => objects}) do
-        render_many(objects, __MODULE__, "show.json", as: single_atom())
+      def render("index.json", %{unquote(plural_atom) => models}) do
+        render_many(models, __MODULE__, "show.json", as: single_atom())
       end
 
+      defp render_association(%{} = model), do: model |> Map.to_list() |> Enum.filter(&render_association/1) |> Map.new
+      defp render_association({_name, %NotLoaded{}}), do: false
+      defp render_association({:__meta__, _}), do: false
+      defp render_association(_), do: true
+      
       defoverridable render: 2
     end
   end
