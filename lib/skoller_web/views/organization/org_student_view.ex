6a550525103a859c41bdd8fc2d6d799adc3ff45b
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2020-06-03 13:38:53 -0500

    render fix

--- lib/skoller_web/views/organization/org_student_view.ex
+++ lib/skoller_web/views/organization/org_student_view.ex
@@ -18,7 +18,7 @@ defmodule SkollerWeb.Organization.OrgStudentView do
     }
   end
 
-  def render("index.json", %{models: models}), do: render_many(models, __MODULE__, "show.json")
+  def render("index.json", %{models: models}), do: render_many(models, __MODULE__, "show.json", as: :model)
 
 
   def render(name, body) do
