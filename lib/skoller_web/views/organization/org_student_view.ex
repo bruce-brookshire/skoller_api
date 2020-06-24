55c9da657d131e62d685146f8a7762ff87965589
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2020-06-24 00:31:49 -0500

    users to user association

--- lib/skoller_web/views/organization/org_student_view.ex
+++ lib/skoller_web/views/organization/org_student_view.ex
@@ -4,10 +4,10 @@ defmodule SkollerWeb.Organization.OrgStudentView do
   use SkollerWeb, :view
 
   def render("show.json", %{model: org_student}) do
-    %{users: users} = student = org_student.student |> Map.take([:name_first, :name_last, :phone, :id, :users])
-    users = Enum.map(users, & render_association(&1))
-
-    student = %{student | users: users}
+    student =
+      org_student.student
+      |> Map.take([:name_first, :name_last, :phone, :id])
+      |> Map.put(:user, org_student.users |> List.first() |> render_association())
 
     %{
       org_group_students: render_association(org_student.org_group_students),
@@ -18,10 +18,11 @@ defmodule SkollerWeb.Organization.OrgStudentView do
     }
   end
 
-  def render("index.json", %{models: models}), do: render_many(models, __MODULE__, "show.json", as: :model)
+  def render("index.json", %{models: models}),
+    do: render_many(models, __MODULE__, "show.json", as: :model)
 
   def render(name, body) do
-    IO.inspect {name, body}
+    IO.inspect({name, body})
     ""
   end
 end
