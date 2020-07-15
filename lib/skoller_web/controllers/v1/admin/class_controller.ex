0fae280a78c9b1e1277b7c72e84b316930b698e1
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2020-07-15 18:05:25 -0500

    insights role on class admin get route

--- lib/skoller_web/controllers/v1/admin/class_controller.ex
+++ lib/skoller_web/controllers/v1/admin/class_controller.ex
@@ -9,9 +9,10 @@ defmodule SkollerWeb.Api.V1.Admin.ClassController do
   import SkollerWeb.Plugs.Auth
   
   @admin_role 200
+  @insights_role 700
   @change_req_role 400
   
-  plug :verify_role, %{roles: [@admin_role, @change_req_role]}
+  plug :verify_role, %{roles: [@admin_role, @change_req_role, @insights_role]}
 
   @doc """
    Shows a single class.
