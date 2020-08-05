704274da6e3b0d15a9634c58d6cec697726115a7
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2020-08-05 14:21:33 -0500

    smh

---
+++ lib/skoller/file_uploaders/org_pics.ex
@@ -0,0 +1,40 @@
+defmodule Skoller.FileUploaders.OrgPics do
+  @moduledoc """
+  # Provides doc upload utilities.
+  # Defines valid extensions, filename, and storage path.
+  # For more information, See `Arc`
+  # S3 buckets are defined in env vars.
+  """
+
+  use Arc.Definition
+
+  # to keep original images as well, add :original to this list.
+  @versions [:thumb]
+  @extensions ~w(.jpg .png .jpeg .gif)
+  @acl :public_read
+
+  @doc false
+  # Whitelist file extensions:
+  def validate({file, _}) do
+    file_extension = file.file_name |> Path.extname() |> String.downcase()
+    Enum.member?(@extensions, file_extension)
+  end
+
+  @doc false
+  # this uses imagemagick to convert the :thumb version images to 200x200 thumbnails.
+  def transform(:thumb, _) do
+    {:convert, "-thumbnail 200x200^ -gravity center -extent 200x200 -format png", :png}
+  end
+
+  @doc false
+  # Override the persisted filenames:
+  def filename(_version, {_file, scope}) do
+    scope["id"]
+  end
+
+  @doc false
+  # Override the storage directory:
+  def storage_dir(_, _) do
+    "uploads/org-thumbs/"
+  end
+end
