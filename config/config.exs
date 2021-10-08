0679d52561a9c517fc3b27c3bf8763c5b91be6c9
Kirill Shamardin <shamardin.k@gmail.com>
2021-10-08 17:45:12 +0700

    set json_library to fix warning

--- config/config.exs
+++ config/config.exs
@@ -58,6 +58,8 @@ config :pigeon, :fcm,
     key: System.get_env("FCM_KEY")
   }
 
+config :phoenix, :json_library, Poison
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
