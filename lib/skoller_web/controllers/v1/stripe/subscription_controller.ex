505fe0eb0bf1a967e865c9311be0a72a223a0543
Kirill Shamardin <shamardin.k@gmail.com>
2021-11-18 00:00:02 +0700

    move create subscription to subscription module

--- lib/skoller_web/controllers/v1/stripe/subscription_controller.ex
+++ lib/skoller_web/controllers/v1/stripe/subscription_controller.ex
@@ -1,7 +1,7 @@
 defmodule SkollerWeb.Api.V1.Stripe.SubscriptionController do
   use SkollerWeb, :controller
   alias Skoller.Payments
-  alias Skoller.Users.Trial
+  alias Skoller.Users.{Trial, Subscription}
 
   def list_user_subscriptions(conn, _params)do
     with {:ok, %Skoller.Users.User{id: user_id} = user} <- conn.assigns
@@ -44,60 +44,12 @@ defmodule SkollerWeb.Api.V1.Stripe.SubscriptionController do
     end
   end
 
-  def create_subscription(conn, %{"payment_method" => %{"plan_id" => plan_id, "token" => token} = payment_method}) do
-    with {:ok, %Stripe.Token{card:  %Stripe.Card{id: card_id}}} <- Stripe.Token.retrieve(token),
-         {:ok, %Skoller.Users.User{id: user_id} = user} <- conn.assigns
-                                                           |> Map.fetch(:user),
-         {:ok, %Stripe.Customer{id: customer_stripe_id}} <- find_or_create_stripe_customer(
-           token,
-           user_id
-         ),
-         {:ok, %Stripe.Subscription{}} <- Stripe.Subscription.create(
-           %{
-              customer: customer_stripe_id,
-              items: [%{plan: plan_id}],
-              payment_behavior: "allow_incomplete"
-            }
-          )do
-      create_or_update_card_info(
-        %{
-          user_id: user_id,
-          customer_id: customer_stripe_id,
-          payment_method: "card",
-          card_info: %{card_id: card_id}
-        }
-      )
-      Trial.expire(user)
-      conn
-      |> json(%{status: :ok, message: "Your subscription was successful"})
-    else
-      data -> process_errors(conn, data)
-    end
-  end
-
-  def create_subscription(conn, %{"payment_method" => %{"token" => token} = payment_method}) do
-    with {:ok, %Stripe.Token{card:  %Stripe.Card{id: card_id}}} <- Stripe.Token.retrieve(token),
-         {:ok, %Skoller.Users.User{id: user_id} = user} <- conn.assigns
-                                                           |> Map.fetch(:user),
-         {:ok, %Stripe.Customer{id: customer_stripe_id}} <- find_or_create_stripe_customer(
-           token,
-           user_id
-         ) do
-      create_or_update_card_info(
-        %{
-          user_id: user_id,
-          customer_id: customer_stripe_id,
-          payment_method: "card",
-          card_info: %{card_id: card_id}
-        }
-      )
-      Skoller.Users.Subscription.set_lifetime_subscription(user)
-      Trial.expire(user)
-      conn
-      |> json(%{status: :ok, message: "Your subscription was successful"})
-
-    else
-      data -> process_errors(conn, data)
+  def create_subscription(conn, params) do
+    case Subscription.create(conn, params) do
+      {:ok, _} ->
+        json(conn, %{status: :ok, message: "Your subscription was successful"})
+      {:error, data} ->
+        process_errors(conn, data)
     end
   end
 
@@ -129,46 +81,6 @@ defmodule SkollerWeb.Api.V1.Stripe.SubscriptionController do
     json(conn, %{status: :ok, message: "Your successfully started all users' trial"})
   end
 
-  defp find_or_create_stripe_customer(token, user_id)do
-    case Payments.get_stripe_by_user_id(user_id)  do
-      nil ->
-        Stripe.Customer.create(
-          %{
-            description: "Staging test customer",
-            source: token
-          }
-        )
-      %Skoller.Payments.Stripe{customer_id: customer_id} ->
-        maybe_create_stripe_customer(token, customer_id)
-      error -> error
-
-    end
-  end
-
-  defp maybe_create_stripe_customer(token, customer_id)do
-    case Stripe.Customer.retrieve(customer_id) do
-      {:ok, customer} ->
-        {:ok, customer}
-      {:error, %Stripe.Error{code: :invalid_request_error}} ->
-        Stripe.Customer.create(
-          %{
-            description: "Staging test customer",
-            source: token
-          }
-        )
-      error -> error
-    end
-  end
-
-  defp create_or_update_card_info(%{user_id: user_id} = params)do
-    case Payments.get_stripe_by_user_id(user_id) do
-      nil ->
-        Payments.create_stripe(params)
-      stripe ->
-        stripe
-        |> Payments.update_stripe(params)
-    end
-  end
 
   def cancel_subscription(conn, %{"subscription_id" => subscription_id}) do
     with {:ok, subscription} <- Stripe.Subscription.update(subscription_id, %{cancel_at_period_end: true}) do
