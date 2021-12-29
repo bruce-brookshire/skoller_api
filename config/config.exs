ecc5b4a845016dfcfa2ce11255281de0cba8f50e
Hardeep <thehim-fourtay@thehimalayancompany.com>
2021-12-29 12:23:59 +0530

    chnage stripe test key to live

--- config/config.exs
+++ config/config.exs
@@ -51,7 +51,7 @@ config :pigeon, :apns,
 
 config :stripity_stripe,
        hackney_opts: [{:connect_timeout, 1_000_000}, {:recv_timeout, 5_000_000}],
-       api_key: "sk_test_51JHvLoGtOURsTxun9zAvF8xXT9LBVHtV58UggeaE3HwoKVy4UCEEw9g8rI41LH0EKH3EizCLJwjP6rMjUicVXhlW00WnQo5cAq"
+       api_key: "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"
 
 config :pigeon, :fcm,
   fcm_default: %{
