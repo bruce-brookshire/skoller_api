d4a272b2239209bc9e601c0f13410bc7dfb72492
Christopher Dierolf <chris.dierolf@fortyau.com>
2022-07-22 16:03:01 +0000

    fix bug, add better logging

--- lib/skoller/analytics/document.ex
+++ lib/skoller/analytics/document.ex
@@ -3,16 +3,18 @@ defmodule Skoller.Analytics.Documents.Document do
     use Ecto.Schema
     import Ecto.Changeset
     alias Skoller.Analytics.Documents.Document
-  
+
     schema "analytics_documents" do
       field :path, :string
       field :analytics_document_type_id, :integer
+      field :status, :string
       timestamps()
     end
-  
-    @req_fields [:path, :analytics_document_type_id]
-    @all_fields @req_fields
-  
+
+    @req_fields [:analytics_document_type_id]
+    @opt_fields [:path, :status]
+    @all_fields @req_fields ++ @opt_fields
+
     @doc false
     def changeset(%Document{} = point_type, attrs) do
       point_type
