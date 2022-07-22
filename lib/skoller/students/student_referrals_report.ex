d0896fc2e87e6da06e109d889f54b5f27603c918
Christopher Dierolf <chris.dierolf@fortyau.com>
2022-07-22 21:08:29 +0000

    calculate commissions and collections from enrolled students

--- lib/skoller/students/student_referrals_report.ex
+++ lib/skoller/students/student_referrals_report.ex
@@ -41,9 +41,8 @@ defmodule Skoller.Students.StudentReferralsReport do
     |> Enum.to_list()
     |> add_headers()
     |> to_string()
-    |> IO.inspect
-    # |> upload_document(file_path, scope)
-    # |> store_document(scope)
+    |> upload_document(file_path, scope)
+    |> store_document(scope)
   end
 
   defp subscriptions do
@@ -63,47 +62,54 @@ defmodule Skoller.Students.StudentReferralsReport do
     |> Enum.map(&(&1.customer))
   end
 
-  defp active_users_plans(subscriptions) do
-    subscriptions
-    |> Enum.reject(& &1.status != "active")
-  end
-
   defp gather_collections_and_commissions(query_results, subscriptions) do
-    query_results =
     Enum.reduce(query_results, [], fn result, acc ->
-
       [
         result
         |> Map.put(:collections, get_collections(result.referring_student, subscriptions))
-       # |> Map.put(:commissions, get_commissions(result.referring_student, subscriptions))
 
         | acc
       ]
     end)
-    |> IO.inspect
   end
 
   defp get_collections(referring_student, subscriptions) do
     Map.get(referring_student, :enrolled_students, nil)
     |> then(fn enrolled_students ->
       if !is_nil(enrolled_students) do
-        Enum.reduce(enrolled_students, [], fn enrolled_student, acc ->
-          collection =
+        collection =
+        Enum.reduce(enrolled_students, 0, fn enrolled_student, acc ->
           if !is_nil(enrolled_student.user.customer_info) do
             subscription = Enum.find(subscriptions, nil, & &1.customer == enrolled_student.user.customer_info.customer_id)
-            commission = calculate_commission(subscription)
-            |> IO.inspect
+            calculate_collection(subscription)
           else
-            0
-          end
-
-
+            0.0
+          end + acc
         end)
+
+        commission =
+          Enum.reduce(enrolled_students, 0, fn enrolled_student, acc ->
+            if !is_nil(enrolled_student.user.customer_info) do
+              subscription = Enum.find(subscriptions, nil, & &1.customer == enrolled_student.user.customer_info.customer_id)
+              calculate_commission(subscription)
+            else
+              0.0
+            end + acc
+          end)
+
+          %{
+            collection: collection |> :erlang.float_to_binary([decimals: 2]),
+            commission: commission |> :erlang.float_to_binary([decimals: 2])
+          }
       end
     end)
   end
 
-  defp calculate_commission(%{plan: %{amount: amount}}) when amount > 0, do: amount / 100
+  defp calculate_collection(%{plan: %{amount: amount}}) when amount > 0, do: amount / 100
+  defp calculate_collection(%{plan: %{amount: 0}}), do: 0
+  defp calculate_collection(nil), do: 0
+
+  defp calculate_commission(%{plan: %{amount: amount}}) when amount > 0, do: (amount / 100) * (1.0 / 3.0)
   defp calculate_commission(%{plan: %{amount: 0}}), do: 0
   defp calculate_commission(nil), do: 0
 
@@ -118,8 +124,8 @@ defmodule Skoller.Students.StudentReferralsReport do
           data.trial,
           data.expired,
           data.premium,
-          data.collections,
-          data.commissions,
+          data.collections.collection,
+          data.collections.commission,
           data.venmo_handle
         ]
         | acc
@@ -162,7 +168,7 @@ defmodule Skoller.Students.StudentReferralsReport do
     Documents.set_current_student_referrals_csv_path(path, %{status: "success"})
   end
 
-  defp store_document({:error, error}, %{:dir => "student_referrals_csv"} = scope) do
+  defp store_document({:error, error}, %{:dir => "student_referrals_csv"}) do
     Logger.info("Failed to store Student Referrals Report")
     Documents.set_current_student_referrals_csv_path(nil, %{status: Atom.to_string(error)})
   end
