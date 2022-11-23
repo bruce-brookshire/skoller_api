73befe34ac8689ccb5cde7df9d282e4e4397e01d
Christopher Dierolf <chris.dierolf@fortyau.com>
2022-11-23 20:25:30 +0000

    typo

--- lib/skoller/contexts/subscriptions/apple/in_app_purchases.ex
+++ lib/skoller/contexts/subscriptions/apple/in_app_purchases.ex
@@ -166,7 +166,7 @@ defmodule Skoller.Contexts.Subscriptions.Apple.InAppPurchases do
   defp get_cancel_at_for_creation(created_at, :month, expiration_intent)
     when not is_nil(expiration_intent) do
     created_at
-    |> DateTimem.from_unix!()
+    |> DateTime.from_unix!()
     |> DateTime.to_naive()
     |> Timex.shift(months: 1)
     |> Timex.to_unix()
